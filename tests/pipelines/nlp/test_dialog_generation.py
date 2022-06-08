b698506a2c7a8ca8b8cc7b2bf7cc3bb23b6bb8de
ly119399 <ly119399@alibaba-inc.com>
2022-06-08 23:12:59 +0800

    model forward ready

--- tests/pipelines/nlp/test_dialog_generation.py
+++ tests/pipelines/nlp/test_dialog_generation.py
@@ -26,18 +26,19 @@ class DialogGenerationTest(unittest.TestCase):
             model_dir=modeldir,
             text_field=preprocessor.text_field,
             config=preprocessor.config)
-        # pipeline = DialogGenerationPipeline(model, preprocessor)
-
-        history_dialog = {}
-        for step, item in enumerate(test_case['sng0073']['log']):
-            user_question = item['user']
-            print('user: {}'.format(user_question))
-
-            # history_dialog_info = merge(history_dialog_info,
-            #                             result) if step > 0 else {}
-            # result = pipeline(user_question, history=history_dialog_info)
-            #
-            # print('sys : {}'.format(result['pred_answer']))
+        print(model.forward(None))
+        # pipeline = DialogGenerationPipeline(model=model, preprocessor=preprocessor)
+        #
+        # history_dialog_info = {}
+        # for step, item in enumerate(test_case['sng0073']['log']):
+        #     user_question = item['user']
+        #     print('user: {}'.format(user_question))
+        #
+        #     # history_dialog_info = merge(history_dialog_info,
+        #     #                             result) if step > 0 else {}
+        #     result = pipeline(user_question, history=history_dialog_info)
+        #     #
+        #     # print('sys : {}'.format(result['pred_answer']))
 
 
 if __name__ == '__main__':

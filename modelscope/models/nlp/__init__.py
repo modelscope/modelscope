e4f2bdce548aa1377718c846f3f15d1fc8ce998d
ly119399 <ly119399@alibaba-inc.com>
2022-06-13 19:03:37 +0800

    bug fix

--- modelscope/models/nlp/__init__.py
+++ modelscope/models/nlp/__init__.py
@@ -1,4 +1,3 @@
 from .sequence_classification_model import *  # noqa F403
-from .space.dialog_generation_model import *  # noqa F403
 from .space.dialog_intent_model import *  # noqa F403
 from .text_generation_model import *  # noqa F403
--- modelscope/preprocessors/__init__.py
+++ modelscope/preprocessors/__init__.py
@@ -6,5 +6,4 @@ from .common import Compose
 from .image import LoadImage, load_image
 from .nlp import *  # noqa F403
 from .nlp import TextGenerationPreprocessor
-from .space.dialog_generation_preprocessor import *  # noqa F403
 from .space.dialog_intent_preprocessor import *  # noqa F403

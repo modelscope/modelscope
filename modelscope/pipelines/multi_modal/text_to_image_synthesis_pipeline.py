38a4a4fdb13b6afd5e0d72974b0b1e197e81767f
xuangen.hlh <xuangen.hlh@alibaba-inc.com>
2022-07-06 07:22:50 +0800

    [to #42322933] Update cv-person-image-cartoon-pipeline to maas lib         Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/9268865

--- modelscope/pipelines/multi_modal/text_to_image_synthesis_pipeline.py
+++ modelscope/pipelines/multi_modal/text_to_image_synthesis_pipeline.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict
 
+import torch
+
 from modelscope.metainfo import Pipelines
 from modelscope.pipelines.base import Input
 from modelscope.utils.constant import Tasks
@@ -16,16 +18,17 @@ logger = get_logger()
     module_name=Pipelines.text_to_image_synthesis)
 class TextToImageSynthesisPipeline(Pipeline):
 
-    def __init__(self, model: str, device_id: int = -1):
+    def __init__(self, model: str, **kwargs):
+        device_id = 0 if torch.cuda.is_available() else -1
         if isinstance(model, str):
-            pipe_model = Model.from_pretrained(model)
+            pipe_model = Model.from_pretrained(model, device_id=device_id)
         elif isinstance(model, Model):
             pipe_model = model
         else:
             raise NotImplementedError(
                 f'expecting a Model instance or str, but get {type(model)}.')
 
-        super().__init__(model=pipe_model)
+        super().__init__(model=pipe_model, **kwargs)
 
     def preprocess(self, input: Input) -> Dict[str, Any]:
         return input

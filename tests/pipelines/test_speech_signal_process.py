3cbc04c6f6b61565c9450a4bcd3f1bf892f1247d
bin.xue <bin.xue@alibaba-inc.com>
2022-07-05 19:50:55 +0800

    [to #43003827] update url of aec lib         Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/9276877
        * update url of aec lib

--- tests/pipelines/test_speech_signal_process.py
+++ tests/pipelines/test_speech_signal_process.py
@@ -13,8 +13,7 @@ FAREND_SPEECH_URL = 'https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/AEC/
 NEAREND_MIC_FILE = 'nearend_mic.wav'
 FAREND_SPEECH_FILE = 'farend_speech.wav'
 
-AEC_LIB_URL = 'http://isv-data.oss-cn-hangzhou.aliyuncs.com/ics%2FMaaS%2FAEC%2Flib%2Flibmitaec_pyio.so' \
-              '?Expires=1664085465&OSSAccessKeyId=LTAIxjQyZNde90zh&Signature=Y7gelmGEsQAJRK4yyHSYMrdWizk%3D'
+AEC_LIB_URL = 'https://modelscope.oss-cn-beijing.aliyuncs.com/dependencies/ics_MaaS_AEC_lib_libmitaec_pyio.so'
 AEC_LIB_FILE = 'libmitaec_pyio.so'
 
 NOISE_SPEECH_URL = 'https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ANS/sample_audio/speech_with_noise.wav'

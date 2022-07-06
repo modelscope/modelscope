9b7e68b67ee715b905764dcd58f5276765ecf2c1
jiaqi.sjq <jiaqi.sjq@alibaba-inc.com>
2022-07-06 13:20:04 +0800

    [to #9285266] tts pipeline using model id params
    Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/9285266
            Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/9285266

--- modelscope/pipelines/audio/text_to_speech_pipeline.py
+++ modelscope/pipelines/audio/text_to_speech_pipeline.py
@@ -9,7 +9,8 @@ from modelscope.models.audio.tts.am import SambertNetHifi16k
 from modelscope.models.audio.tts.vocoder import Hifigan16k
 from modelscope.pipelines.base import Pipeline
 from modelscope.pipelines.builder import PIPELINES
-from modelscope.preprocessors import TextToTacotronSymbols, build_preprocessor
+from modelscope.preprocessors import (Preprocessor, TextToTacotronSymbols,
+                                      build_preprocessor)
 from modelscope.utils.constant import Fields, Tasks
 
 __all__ = ['TextToSpeechSambertHifigan16kPipeline']
@@ -20,19 +21,19 @@ __all__ = ['TextToSpeechSambertHifigan16kPipeline']
 class TextToSpeechSambertHifigan16kPipeline(Pipeline):
 
     def __init__(self,
-                 config_file: str = None,
-                 model: List[Model] = None,
-                 preprocessor: TextToTacotronSymbols = None,
+                 model: List[str] = None,
+                 preprocessor: Preprocessor = None,
                  **kwargs):
-        super().__init__(
-            config_file=config_file,
-            model=model,
-            preprocessor=preprocessor,
-            **kwargs)
-        assert len(model) == 2, 'model number should be 2'
-        self._am = model[0]
-        self._vocoder = model[1]
-        self._preprocessor = preprocessor
+        assert len(model) == 3, 'model number should be 3'
+        if preprocessor is None:
+            lang_type = 'pinyin'
+            if 'lang_type' in kwargs:
+                lang_type = kwargs.lang_type
+            preprocessor = TextToTacotronSymbols(model[0], lang_type=lang_type)
+        models = [model[1], model[2]]
+        super().__init__(model=models, preprocessor=preprocessor, **kwargs)
+        self._am = self.models[0]
+        self._vocoder = self.models[1]
 
     def forward(self, inputs: Dict[str, Any]) -> Dict[str, np.ndarray]:
         texts = inputs['texts']

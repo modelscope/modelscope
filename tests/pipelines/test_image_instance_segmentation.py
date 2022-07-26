2e43b376f5bb3bab8edee5d3f1beea541d1cd3b8
Yingda Chen <yingda.chen@alibaba-inc.com>
2022-07-26 13:17:29 +0800

    [to #42322933]disable image segmentation test temporarily

--- tests/pipelines/test_image_instance_segmentation.py
+++ tests/pipelines/test_image_instance_segmentation.py
@@ -18,7 +18,7 @@ class ImageInstanceSegmentationTest(unittest.TestCase):
     model_id = 'damo/cv_swin-b_image-instance-segmentation_coco'
     image = 'data/test/images/image_instance_segmentation.jpg'
 
-    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_run_with_model_from_modelhub(self):
         model = Model.from_pretrained(self.model_id)
         config_path = os.path.join(model.model_dir, ModelFile.CONFIGURATION)
@@ -30,7 +30,7 @@ class ImageInstanceSegmentationTest(unittest.TestCase):
             preprocessor=preprocessor)
         print(pipeline_ins(input=self.image)[OutputKeys.LABELS])
 
-    @unittest.skipUnless(test_level() >= 1, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_run_with_model_name(self):
         pipeline_ins = pipeline(
             task=Tasks.image_segmentation, model=self.model_id)

df4f1a538d5d755f0bba372b1ab04b200ac44a49
Yingda Chen <yingda.chen@alibaba-inc.com>
2022-08-19 14:01:06 +0800

    [to #42322933]disable ocr test temporarily

--- tests/pipelines/test_ocr_recognition.py
+++ tests/pipelines/test_ocr_recognition.py
@@ -26,12 +26,12 @@ class OCRRecognitionTest(unittest.TestCase):
         result = pipeline(input_location)
         print('ocr recognition results: ', result)
 
-    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_run_with_model_from_modelhub(self):
         ocr_recognition = pipeline(Tasks.ocr_recognition, model=self.model_id)
         self.pipeline_inference(ocr_recognition, self.test_image)
 
-    @unittest.skipUnless(test_level() >= 1, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_run_with_model_from_modelhub_PILinput(self):
         ocr_recognition = pipeline(Tasks.ocr_recognition, model=self.model_id)
         imagePIL = PIL.Image.open(self.test_image)

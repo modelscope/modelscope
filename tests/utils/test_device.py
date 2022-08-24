c72e5f4ae8bddc4b83e9c1cd7d937340c10e987d
wenmeng.zwm <wenmeng.zwm@alibaba-inc.com>
2022-08-24 15:08:22 +0800

    [to #43878347] skip device placement test
    skip this test which will result in too much debug log for placement although debug level is canceled after this test case
    
    Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/9875987

--- tests/utils/test_device.py
+++ tests/utils/test_device.py
@@ -81,6 +81,7 @@ class DeviceTest(unittest.TestCase):
         with device_placement(Frameworks.torch, 'cpu'):
             pass
 
+    @unittest.skip('skip this test to avoid debug logging.')
     def test_device_placement_tf_gpu(self):
         tf.debugging.set_log_device_placement(True)
         with device_placement(Frameworks.tf, 'gpu:0'):

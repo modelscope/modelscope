562ab9ed616eaa2a18425d3f0af1a0027fadd2eb
xixing.tj <xixing.tj@alibaba-inc.com>
2022-09-02 15:11:05 +0800

    add error msg when no text detected

--- modelscope/pipelines/cv/ocr_detection_pipeline.py
+++ modelscope/pipelines/cv/ocr_detection_pipeline.py
@@ -149,6 +149,8 @@ class OCRDetectionPipeline(Pipeline):
     def postprocess(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         rboxes = inputs['combined_rboxes'][0]
         count = inputs['combined_counts'][0]
+        if count==0 or count < rboxes.shape[0]:
+            raise Exception("No text detected")
         rboxes = rboxes[:count, :]
 
         # convert rboxes to polygons and find its coordinates on the original image

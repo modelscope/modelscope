69497c25f574a9cf9a223689fcf6c9c43f4ac1f5
tianchu.gtc <tianchu.gtc@alibaba-inc.com>
2022-09-26 15:49:35 +0800

    [to #42322933]segformer 接入demo接口更改         Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/10253628

--- modelscope/pipelines/cv/easycv_pipelines/segmentation_pipeline.py
+++ modelscope/pipelines/cv/easycv_pipelines/segmentation_pipeline.py
@@ -1,5 +1,10 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
+from typing import Any
+
+import numpy as np
+
 from modelscope.metainfo import Pipelines
+from modelscope.outputs import OutputKeys
 from modelscope.pipelines.builder import PIPELINES
 from modelscope.utils.constant import Tasks
 from .base import EasyCVPipeline
@@ -21,3 +26,22 @@ class EasyCVSegmentationPipeline(EasyCVPipeline):
             model_file_pattern=model_file_pattern,
             *args,
             **kwargs)
+
+    def __call__(self, inputs) -> Any:
+        outputs = self.predict_op(inputs)
+
+        semantic_result = outputs[0]['seg_pred']
+
+        ids = np.unique(semantic_result)[::-1]
+        legal_indices = ids != len(self.predict_op.CLASSES)  # for VOID label
+        ids = ids[legal_indices]
+        segms = (semantic_result[None] == ids[:, None, None])
+        masks = [it.astype(np.int) for it in segms]
+        labels_txt = np.array(self.predict_op.CLASSES)[ids].tolist()
+
+        results = {
+            OutputKeys.MASKS: masks,
+            OutputKeys.LABELS: labels_txt,
+            OutputKeys.SCORES: [0.999 for _ in range(len(labels_txt))]
+        }
+        return results

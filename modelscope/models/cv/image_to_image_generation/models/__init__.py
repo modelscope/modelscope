9842a427ef793a8ff9e4e67a91bd74ec140b19f5
chaojie.mcj <chaojie.mcj@alibaba-inc.com>
2022-10-20 21:12:47 +0800

    modify headers for __init__
    更改部分__init__文件的header license
            Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/10393596

--- modelscope/models/cv/image_to_image_generation/models/__init__.py
+++ modelscope/models/cv/image_to_image_generation/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) Alibaba, Inc. and its affiliates.
+# Copyright 2021-2022 The Alibaba Fundamental Vision Team Authors. All rights reserved.
 from typing import TYPE_CHECKING
 
 from modelscope.utils.import_utils import LazyImportModule

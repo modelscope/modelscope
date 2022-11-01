d881f723bfcf333d669284055270ca240237981d
wenmeng.zwm <wenmeng.zwm@alibaba-inc.com>
2022-11-01 21:28:14 +0800

    update release time

--- modelscope/version.py
+++ modelscope/version.py
@@ -2,4 +2,4 @@
 __version__ = '1.0.1'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2022-11-01 11:07:00'
+__release_datetime__ = '2022-11-01 21:28:00'

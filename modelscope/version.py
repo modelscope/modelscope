40ba97d86d0f9bf1a646ef3855cc792f1055aeed
wenmeng.zwm <wenmeng.zwm@alibaba-inc.com>
2022-11-02 14:24:39 +0800

    bump version to v1.0.2

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2022-11-01 21:28:00'
+__release_datetime__ = '2022-11-02 14:24:00'

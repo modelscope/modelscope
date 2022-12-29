8ca432f2d0f5be866fbc2ff1209e81c4634268a2
许文卿 <xuwenqing@XWQ.local>
2022-12-29 14:25:02 +0800

    fix missing self.

--- modelscope/trainers/cv/image_classifition_trainer.py
+++ modelscope/trainers/cv/image_classifition_trainer.py
@@ -421,7 +421,7 @@ class ImageClassifitionTrainer(BaseTrainer):
         if self.cfg.dataset.classes is None:
             data_root = get_ms_dataset_root(self.eval_dataset)
             classname_path = osp.join(data_root, 'classname.txt')
-            classes = classname_path if osp.exists(classname_pßath) else None
+            classes = classname_path if osp.exists(classname_path) else None
         else:
             classes = self.cfg.dataset.classes
         dataset = MmDataset(

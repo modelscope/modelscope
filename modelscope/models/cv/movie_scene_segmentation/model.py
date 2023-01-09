338a5a4994290239b694ee49a1b851fb16224a9b
shuying.shu <shuying.shu@alibaba-inc.com>
2023-01-10 06:11:54 +0800

    fix bug for batch inference
    具体bug见https://github.com/modelscope/modelscope/issues/66
            Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/11350862

--- modelscope/models/cv/movie_scene_segmentation/model.py
+++ modelscope/models/cv/movie_scene_segmentation/model.py
@@ -1,6 +1,7 @@
 # The implementation here is modified based on BaSSL,
 # originally Apache 2.0 License and publicly avaialbe at https://github.com/kakaobrain/bassl
 
+import math
 import os
 import os.path as osp
 from typing import Any, Dict
@@ -101,7 +102,7 @@ class MovieSceneSegmentationModel(TorchModel):
         inputs = batch['shot_feat']
 
         shot_num = len(sids)
-        cnt = shot_num // bs + 1
+        cnt = math.ceil(shot_num / bs)
 
         infer_sid, infer_pred = [], []
         infer_result = {}

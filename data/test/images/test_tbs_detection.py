9b43ccd501787b130f4d2aa87adf9aa2193efadf
QZStudio <1643006760@qq.com>
2023-02-01 14:34:13 +0800

    commit message

---
+++ data/test/images/test_tbs_detection.py
@@ -0,0 +1,16 @@
+import unittest
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+from modelscope.utils.demo_utils import DemoCompatibilityCheck
+from modelscope.utils.test_utils import test_level
+
+
+class ObjectDetectionTest(unittest.TestCase, DemoCompatibilityCheck):
+
+    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    def test_run_with_model_name(self):
+        tbs_detect = pipeline(Tasks.image_object_detection, model='landingAI/LD_CytoBrainCerv')
+        outputs = tbs_detect(input='data/test/images/tbs_detection.jpg')
+        print(outputs)
+if __name__ == '__main__':
+    unittest.main()

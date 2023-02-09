82ae63b9e76d712958118701c1ea24340dc1d192
liaojie.laj <liaojie.laj@alibaba-inc.com>
2023-02-09 10:45:32 +0000

    improve video frame interpolation pipeline
    更新实用版插帧模型，支持任意帧率转换
    
    Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/11553883

--- modelscope/pipelines/cv/video_frame_interpolation_pipeline.py
+++ modelscope/pipelines/cv/video_frame_interpolation_pipeline.py
@@ -525,8 +525,11 @@ class VideoFrameInterpolationPipeline(Pipeline):
         logger.info('load video frame-interpolation done')
 
     def preprocess(self, input: Input, out_fps: float = 0) -> Dict[str, Any]:
-        # input is a video file
-        video_reader = VideoReader(input)
+        # Determine the input type
+        if isinstance(input, str):
+            video_reader = VideoReader(input)
+        elif isinstance(input, dict):
+            video_reader = VideoReader(input['video'])
         inputs = []
         for frame in video_reader:
             inputs.append(frame)
@@ -536,8 +539,15 @@ class VideoFrameInterpolationPipeline(Pipeline):
             img = torch.from_numpy(img.copy()).permute(2, 0, 1).float()
             inputs[i] = img.unsqueeze(0)
 
-        if out_fps == 0:
+        if isinstance(input, str):
             out_fps = 2 * fps
+        elif isinstance(input, dict):
+            if 'interp_ratio' in input:
+                out_fps = input['interp_ratio'] * fps
+            elif 'out_fps' in input:
+                out_fps = input['out_fps']
+            else:
+                out_fps = 2 * fps
         return {'video': inputs, 'fps': fps, 'out_fps': out_fps}
 
     def forward(self, input: Dict[str, Any]) -> Dict[str, Any]:

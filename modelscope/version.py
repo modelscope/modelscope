ea9bd3cbdf6d08e4e2ae72a4395ffc1cd500b7ad
Zhicheng Zhang <zhangzhicheng.zzc@alibaba-inc.com>
2023-02-15 16:18:09 +0800

    update release version

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.3.0rc0'
+__version__ = '1.3.0'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
 __release_datetime__ = '2023-02-15 16:00:00'

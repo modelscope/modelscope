ebb52e4f2f172ccbefd8b205e4c867d8b30373d6
wenmeng.zwm <wenmeng.zwm@alibaba-inc.com>
2023-02-28 16:41:44 +0800

    bump version to 1.3.1

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2023-02-15 16:00:00'
+__release_datetime__ = '2023-03-01 23:00:00'

060f7ba5402d6161e2aa66d4af60834b722eea38
yzhao <yuze.zyz@alibaba-inc.com>
2023-03-15 22:01:02 +0800

    update version to 1.4.1

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2023-03-15 08:00:00'
+__release_datetime__ = '2023-03-16 08:00:00'

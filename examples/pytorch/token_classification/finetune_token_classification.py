0e9050085109b1509cb864078d8afcfdb47c4882
slin000111 <zhaoshilin2015@sina.com>
2023-04-10 20:11:53 +0800

    token classification

--- examples/pytorch/token_classification/finetune_token_classification.py
+++ examples/pytorch/token_classification/finetune_token_classification.py
@@ -1,28 +1,29 @@
-import os
 from dataclasses import dataclass, field
-from functools import reduce
+from modelscope.metainfo import Trainers
 from modelscope.msdatasets import MsDataset
 from modelscope.trainers import build_trainer
-from modelscope.trainers.training_args import (TrainingArgs,get_flatten_value,set_flatten_value)
-
-def reducer(x, y):
-    x = x.split(' ') if isinstance(x, str) else x
-    y = y.split(' ') if isinstance(y, str) else y
-    return x + y
-
-def get_label_list(labels):
-    label_enumerate_values = list(
-        set(reduce(reducer, labels)))
-    label_enumerate_values.sort()
-    return label_enumerate_values
+from modelscope.trainers.training_args import (TrainingArgs, get_flatten_value,set_flatten_value)
 
 @dataclass
 class TokenClassificationArguments(TrainingArgs):
 
-    name_space:str=field(
+    trainer: str = field(
+        default=Trainers.default, metadata={
+            'help': 'The trainer used',
+        })
+
+    preprocessor: str = field(
         default=None,
         metadata={
-            'help': 'The namespace key of dataset',
+            'help': 'The preprocessor type',
+            'cfg_node': 'preprocessor.type'
+        })
+
+    padding: str = field(
+        default=None,
+        metadata={
+            'help': 'The preprocessor padding',
+            'cfg_node': 'preprocessor.padding'
         })
 
     train_dataset_params: str = field(
@@ -34,45 +35,55 @@ class TokenClassificationArguments(TrainingArgs):
             'help': 'The parameters for train dataset',
         })
 
-    preprocessor: str = field(
-        default=None,
+    work_dir: str = field(
+        default='./tmp',
         metadata={
-            'cfg_node': 'preprocessor.type',
-            'help': 'The preprocessor type'
-        })
-
-    padding: str = field(
-        default=None,
-        metadata={
-            'cfg_node': 'preprocessor.padding',
-            'help': 'The preprocessor padding'
+            'help': 'The working path for saving checkpoint',
         })
 
     def __call__(self, config):
         config = super().__call__(config)
         if config.safe_get('dataset.train.label') == 'ner_tags':
-            label_enumerate_values = get_label_list(train_dataset['ner_tags']+validation_dataset['ner_tags'])
-            config.merge_from_dict({'dataset.train.labels':label_enumerate_values})
+            label_enumerate_values = self._get_label_list(self.ner_tags_labels)
+            config.merge_from_dict(
+                {'dataset.train.labels': label_enumerate_values})
         if config.train.lr_scheduler.type == 'LinearLR':
             config.train.lr_scheduler['total_iters'] = \
-                int(len(train_dataset) / self.per_device_train_batch_size) * self.max_epochs
+                int(len(self.train_dataset) / self.per_device_train_batch_size) * self.max_epochs
         return config
 
-args = TokenClassificationArguments.from_cli(
-    task='token-classification', eval_metrics='token-cls-metric')
+    @staticmethod
+    def _get_label_list(labels):
+        unique_labels = set()
+        for label in labels:
+            unique_labels = unique_labels | set(label)
+        label_list = list(unique_labels)
+        label_list.sort()
+        return label_list
 
+args = TokenClassificationArguments.from_cli(task='token-classification',eval_metrics='token-cls-metric')
 print(args)
 
-train_dataset=MsDataset.load(args.dataset_name, subset_name=args.subset_name,namespace=args.name_space,split='train')['train']
-validation_dataset=MsDataset.load(args.dataset_name, subset_name=args.subset_name,namespace=args.name_space,split='validation')['validation']
+train_dataset = MsDataset.load(
+    args.dataset_name,
+    subset_name=args.subset_name,
+    split='train',
+    namespace='damo')['train']
+eval_dataset = MsDataset.load(
+    args.dataset_name,
+    subset_name=args.subset_name,
+    split='validation',
+    namespace='damo')['validation']
+
+args.ner_tags_labels = train_dataset['ner_tags'] + eval_dataset['ner_tags']
+args.train_dataset = train_dataset
 
 kwargs = dict(
     model=args.model,
     train_dataset=train_dataset,
-    eval_dataset=validation_dataset,
-    seed=args.seed,
+    eval_dataset=eval_dataset,
     work_dir=args.work_dir,
     cfg_modify_fn=args)
 
-trainer = build_trainer(name='nlp_base_trainer', default_args=kwargs)
+trainer = build_trainer(name=args.trainer, default_args=kwargs)
 trainer.train()
--- examples/pytorch/token_classification/run_finetune.sh
+++ examples/pytorch/token_classification/run_finetune.sh
@@ -1,28 +1,15 @@
 PYTHONPATH=. torchrun examples/pytorch/token_classification/finetune_token_classification.py \
+    --trainer 'nlp-base-trainer' \
     --model 'damo/mgeo_backbone_chinese_base' \
     --dataset_name 'GeoGLUE' \
     --subset_name 'GeoETA' \
-    --name_space 'damo' \
-    --train_dataset_param 'first_sequence=tokens,label=ner_tags,sequence_length=128' \
+    --train_dataset_params 'first_sequence=tokens,label=ner_tags,sequence_length=128' \
     --preprocessor 'token-cls-tokenizer' \
     --padding 'max_length' \
-    --per_device_train_batch_size 32 \
     --max_epochs 1 \
+    --per_device_train_batch_size 32 \
     --lr 3e-5 \
-    --logging_interval 100 \
-    --eval_strategy 'by_epoch' \
+    --work_dir './tmp' \
     --save_ckpt_strategy 'by_epoch' \
-    --work_dir './tmp'
-
-
-
-
-
-
-
-
-
-
-
-
-
+    --logging_interval 10 \
+    --eval_strategy 'by_epoch' \

595a85931026a23ca79f4c3139450d1f9b7e3d73
hemu <hemu.zp@alibaba-inc.com>
2023-04-22 10:41:36 +0800

    update version to 1.5.2

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
 __release_datetime__ = '2023-04-18 08:00:00'

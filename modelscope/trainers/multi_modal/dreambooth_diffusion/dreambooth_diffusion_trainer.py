08908d8d5907ccf4aa05aa83c82e33dd4f8f159c
XDUWQ <1300964705@qq.com>
2023-05-26 16:41:58 +0800

    datasets

--- modelscope/trainers/multi_modal/dreambooth_diffusion/dreambooth_diffusion_trainer.py
+++ modelscope/trainers/multi_modal/dreambooth_diffusion/dreambooth_diffusion_trainer.py
@@ -272,31 +272,11 @@ class DreamboothDiffusionTrainer(EpochBasedTrainer):
         return prompt_embeds
     
     def data_assemble(self, inputs, with_prior_preservation=False):
-        examples = {}
-        examples["instance_images"] = inputs["target"]
+        batch = {}
+        batch["pixel_values"] = inputs["target"]
         text_inputs = self.tokenize_prompt(self.tokenizer, self.instance_prompt)
-        examples["instance_prompt_ids"] = text_inputs.input_ids
-        examples["instance_attention_mask"] = text_inputs.attention_mask
-
-        input_ids = [example["instance_prompt_ids"] for example in examples]
-        pixel_values = [example["instance_images"] for example in examples]
-        attention_mask = [example["instance_attention_mask"] for example in examples]
-
-        # Concat class and instance examples for prior preservation.
-        # We do this to avoid doing two forward passes.
-        if with_prior_preservation:
-            input_ids += [example["class_prompt_ids"] for example in examples]
-            pixel_values += [example["class_images"] for example in examples]
-            attention_mask += [example["class_attention_mask"] for example in examples]
-
-        pixel_values = torch.stack(pixel_values)
-        pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-        input_ids = torch.cat(input_ids, dim=0)
-        batch = {
-            "input_ids": input_ids,
-            "pixel_values": pixel_values,
-            "attention_mask": attention_mask
-        }
+        batch["input_ids"] = text_inputs.input_ids
+        batch["attention_mask"] = text_inputs.attention_mask
 
         return batch
 

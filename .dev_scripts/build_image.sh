25a7398aeab1a36466fd5e5278317f7de9032d19
mulin.lyh <mulin.lyh@taobao.com>
2023-06-08 10:01:58 +0800

    docker file py38 and py37 compatible merge Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/12722824 * debug
    * add missing deps
    
    * add stanza
    
    * fix numpy issue
    
    * numpy version
    
    * tf case
    
    * numpy <= 1.22.0
    
    * fix tf 1.15.5 case
    
    * add python38 and python37 compatible docker file
    
    * cv add fvcore
    
    * add easycv
    
    * remove debug code
    
    * fix style issue
    
    * fix python3.7 python3.8 compatible issue
    
    * comment
    
    * modify run config
    
    * scipy<=1.7.3
    
    * revert scipy<=1.7.3
    
    * fix compatible bug
    
    * fix compatible bug
    
    * add easycv
    
    * update version: 1.6.1rc0
    Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/12754942
    fix python3.8 and python3.8 compatible issue
    
    * numpy version change
    
    * numpy version
    
    * numpy version
    
    * fix numpy version
    
    * fix numpy version
    
    * update version: 1.6.1rc0
    Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/12754942
    fix python3.8 and python3.8 compatible issue
    
    * restore setup.py
    
    * restore setup.py
    
    * add build base image
    
    * fix style
    
    * numpy version fix
    
    * optimize build image, split base and modelscope image
    
    * fix style issue
    
    * modify example path
    
    * modify examples folder
    
    * add SETUPTOOLS_USE_DISTUTILS=stdlib comments

--- .dev_scripts/build_image.sh
+++ .dev_scripts/build_image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # default values.
-BASE_CPU_IMAGE=reg.docker.alibaba-inc.com/modelscope/ubuntu:20.04
-BASE_GPU_IMAGE=reg.docker.alibaba-inc.com/modelscope/ubuntu:20.04-cuda11.3.0-cudnn8-devel
+BASE_PY38_CPU_IMAGE=reg.docker.alibaba-inc.com/modelscope/modelscope:ubuntu20.04-py38-torch1.11.0-tf1.15.5-base-1.6.1
+BASE_PY38_GPU_IMAGE=reg.docker.alibaba-inc.com/modelscope/modelscope:ubuntu20.04-cuda11.3.0-py38-torch1.11.0-tf1.15.5-base-1.6.1
+BASE_PY37_CPU_IMAGE=reg.docker.alibaba-inc.com/modelscope/modelscope:ubuntu20.04-py37-torch1.11.0-tf1.15.5-base-1.6.1
+BASE_PY37_GPU_IMAGE=reg.docker.alibaba-inc.com/modelscope/modelscope:ubuntu20.04-cuda11.3.0-py37-torch1.11.0-tf1.15.5-base-1.6.1
 MODELSCOPE_REPO_ADDRESS=reg.docker.alibaba-inc.com/modelscope/modelscope
 python_version=3.7.13
 torch_version=1.11.0
@@ -86,20 +88,30 @@ if [ "$modelscope_version" == "None" ]; then
     exit 1
 fi
 if [ "$is_cpu" == "True" ]; then
-    export BASE_IMAGE=$BASE_CPU_IMAGE
     base_tag=ubuntu20.04
     export USE_GPU=False
 else
-    export BASE_IMAGE=$BASE_GPU_IMAGE
     base_tag=ubuntu20.04-cuda11.3.0
     export USE_GPU=True
 fi
 if [[ $python_version == 3.7* ]]; then
+    if [ "$is_cpu" == "True" ]; then
+        echo "Building python3.7 cpu image"
+        export BASE_IMAGE=$BASE_PY37_CPU_IMAGE
+    else
+        echo "Building python3.7 gpu image"
+        export BASE_IMAGE=$BASE_PY37_GPU_IMAGE
+    fi
     base_tag=$base_tag-py37
 elif [[ $python_version == 3.8* ]]; then
+    if [ "$is_cpu" == "True" ]; then
+        echo "Building python3.8 cpu image"
+        export BASE_IMAGE=$BASE_PY38_CPU_IMAGE
+    else
+        echo "Building python3.8 gpu image"
+        export BASE_IMAGE=$BASE_PY38_GPU_IMAGE
+    fi
     base_tag=$base_tag-py38
-elif [[ $python_version == 3.9* ]]; then
-    base_tag=$base_tag-py39
 else
     echo "Unsupport python version: $python_version"
     exit 1

a3da55105fcc0e4cc58d9ab17f83db3b0f3ed5bd
XDUWQ <1300964705@qq.com>
2023-06-09 15:32:00 +0800

    pipeline

--- modelscope/pipelines/multi_modal/diffusers_wrapped/stable_diffusion/stable_diffusion_pipeline.py
+++ modelscope/pipelines/multi_modal/diffusers_wrapped/stable_diffusion/stable_diffusion_pipeline.py
@@ -1,6 +1,6 @@
 # Copyright © Alibaba, Inc. and its affiliates.
 
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import cv2
 import numpy as np
@@ -12,6 +12,7 @@ from modelscope.outputs import OutputKeys
 from modelscope.pipelines.base import Pipeline
 from modelscope.pipelines.builder import PIPELINES
 from modelscope.utils.constant import Tasks
+from modelscope.preprocessors import Preprocessor
 
 
 @PIPELINES.register_module(
@@ -19,18 +20,27 @@ from modelscope.utils.constant import Tasks
     module_name=Pipelines.diffusers_stable_diffusion)
 class StableDiffusionPipeline(Pipeline):
 
-    def __init__(self, model: str, **kwargs):
+    def __init__(self, model: str, 
+                  device: str = 'gpu',
+                 config_file: str = None, 
+                 preprocessor: Optional[Preprocessor] = None,
+                 auto_collate=True,
+                 **kwargs):
         """
         use `model` to create a stable diffusion pipeline
         Args:
             model: model id on modelscope hub or local model dir.
         """
-        super().__init__(model, **kwargs)
+        super().__init__(model=model,
+                         device=device,
+                         config_file=config_file,
+                         preprocessor=preprocessor,
+                         auto_collate=auto_collate)
 
         # torch_dtype = kwargs.get('torch_dtype', torch.float32)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = self.model.to(self.device)
-        self.model.eval()
+        # self.model = self.model.to(self.device)
+        # self.model.eval()
     
     def forward(self, inputs: Dict[str, Any],
                 **forward_params) -> Dict[str, Any]:

184f289cd7bb0747f8814928d872e89feaa75af4
XDUWQ <1300964705@qq.com>
2023-06-09 15:00:34 +0800

    pipeline

--- modelscope/pipelines/multi_modal/diffusers_wrapped/stable_diffusion/stable_diffusion_pipeline.py
+++ modelscope/pipelines/multi_modal/diffusers_wrapped/stable_diffusion/stable_diffusion_pipeline.py
@@ -5,65 +5,38 @@ from typing import Any, Dict
 import cv2
 import numpy as np
 import torch
-from diffusers import StableDiffusionPipeline
 from PIL import Image
 
 from modelscope.metainfo import Pipelines
 from modelscope.outputs import OutputKeys
+from modelscope.pipelines.base import Pipeline
 from modelscope.pipelines.builder import PIPELINES
-from modelscope.pipelines.multi_modal.diffusers_wrapped.diffusers_pipeline import \
-    DiffusersPipeline
 from modelscope.utils.constant import Tasks
 
 
-# Wrap around the diffusers stable diffusion pipeline implementation
-# for a unified ModelScope pipeline experience. Native stable diffusion
-# pipelines will be implemented in later releases.
 @PIPELINES.register_module(
     Tasks.text_to_image_synthesis,
     module_name=Pipelines.diffusers_stable_diffusion)
-class StableDiffusionWrapperPipeline(DiffusersPipeline):
+class StableDiffusionPipeline(Pipeline):
 
-    def __init__(self, model: str, device: str = 'gpu', **kwargs):
+    def __init__(self, model: str, **kwargs):
         """
         use `model` to create a stable diffusion pipeline
         Args:
-            model: model id on modelscope hub.
-            device: str = 'gpu'
+            model: model id on modelscope hub or local model dir.
         """
-        super().__init__(model, device, **kwargs)
+        super().__init__(model, **kwargs)
 
-        torch_dtype = kwargs.get('torch_dtype', torch.float32)
-
-        # build upon the diffuser stable diffusion pipeline
-        self.pipeline = StableDiffusionPipeline.from_pretrained(
-            model, torch_dtype=torch_dtype)
-        self.pipeline.to(self.device)
-
+        # torch_dtype = kwargs.get('torch_dtype', torch.float32)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = self.model.to(self.device)
+        self.model.eval()
+    
     def forward(self, inputs: Dict[str, Any],
                 **forward_params) -> Dict[str, Any]:
-        if not isinstance(inputs, dict):
-            raise ValueError(
-                f'Expected the input to be a dictionary, but got {type(input)}'
-            )
-        if 'text' not in inputs:
-            raise ValueError('input should contain "text", but not found')
-
-        return self.pipeline(
-            prompt=inputs.get('text'),
-            height=inputs.get('height'),
-            width=inputs.get('width'),
-            num_inference_steps=inputs.get('num_inference_steps', 50),
-            guidance_scale=inputs.get('guidance_scale', 7.5),
-            negative_prompt=inputs.get('negative_prompt'),
-            num_images_per_prompt=inputs.get('num_images_per_prompt', 1),
-            eta=inputs.get('eta', 0.0),
-            generator=inputs.get('generator'),
-            latents=inputs.get('latents'),
-            output_type=inputs.get('output_type', 'pil'),
-            return_dict=inputs.get('return_dict', True),
-            callback=inputs.get('callback'),
-            callback_steps=inputs.get('callback_steps', 1))
+        with torch.no_grad():
+            results = self.model(**inputs)
+            return results
 
     def postprocess(self, inputs: Dict[str, Any], **kwargs) -> Dict[str, Any]:
         images = []

a8bdc09f8dcca370165c0e2d5ce8179ba8324d15
XDUWQ <1300964705@qq.com>
2023-06-14 11:18:17 +0800

    example

--- examples/pytorch/stable_diffusion/finetune_stable_diffusion.py
+++ examples/pytorch/stable_diffusion/finetune_stable_diffusion.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass, field
-
+from modelscope.metainfo import Trainers
 from modelscope.msdatasets import MsDataset
 from modelscope.trainers import EpochBasedTrainer, build_trainer
 from modelscope.trainers.training_args import TrainingArgs
 
-training_args = TrainingArgs(task='efficient-diffusion-tuning').parse_cli()
+training_args = TrainingArgs(task='lora-diffusion').parse_cli()
 config, args = training_args.to_config()
 print(args)
 
@@ -15,21 +14,23 @@ validation_dataset = dataset['validation']
 
 
 def cfg_modify_fn(cfg):
-    if args.use_model_config:
-        cfg.merge_from_dict(config)
-    else:
-        cfg = config
-    cfg.train.lr_scheduler.T_max = training_args.max_epochs
-    cfg.model.inference = False
+    cfg.train.max_epochs = args.max_epochs
+    cfg.train.lr_scheduler = {
+        'type': 'LambdaLR',
+        'lr_lambda': lambda _: 1,
+        'last_epoch': -1
+    }
+    cfg.train.optimizer.lr = 1e-4
     return cfg
 
 
 kwargs = dict(
     model=training_args.model,
+    model_revision=args.model_revision,
     work_dir=training_args.work_dir,
     train_dataset=train_dataset,
     eval_dataset=validation_dataset,
     cfg_modify_fn=cfg_modify_fn)
 
-trainer: EpochBasedTrainer = build_trainer(name='trainer', default_args=kwargs)
+trainer: EpochBasedTrainer = build_trainer(name=Trainers.lora_diffusion, default_args=kwargs)
 trainer.train()
--- examples/pytorch/stable_diffusion/run_train.sh
+++ examples/pytorch/stable_diffusion/run_train.sh
@@ -1,12 +1,12 @@
 PYTHONPATH=. torchrun examples/pytorch/stable_diffusion/finetune_stable_diffusion.py \
-    --model 'damo/multi-modal_efficient-diffusion-tuning-lora' \
-    --work_dir './tmp/stable_diffusion_tuning' \
-    --train_dataset_namespace 'damo' \
-    --train_dataset_name 'controlnet_dataset_condition_fill50k' \
-    --max_epochs 1 \
+    --model 'AI-ModelScope/stable-diffusion-v1-5' \
+    --model_revision 'v1.0.6' \
+    --work_dir './tmp/lora_diffusion' \
+    --train_dataset_name 'buptwq/lora-stable-diffusion-finetune' \
+    --max_epochs 100 \
     --save_ckpt_strategy 'by_epoch' \
     --logging_interval 100 \
     --train.dataloader.workers_per_gpu 0 \
     --evaluation.dataloader.workers_per_gpu 0 \
-    --train.optimizer.lr 1e-5 \
+    --train.optimizer.lr 1e-4 \
     --use_model_config true

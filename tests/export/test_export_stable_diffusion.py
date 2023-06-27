89ff2f4d43d78b7c3f2e33647206338c11cedc4c
XDUWQ <1300964705@qq.com>
2023-06-27 17:40:13 +0800

    test export stable diffusion

---
+++ tests/export/test_export_stable_diffusion.py
@@ -0,0 +1,30 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+import os
+import shutil
+import tempfile
+import unittest
+from collections import OrderedDict
+
+from modelscope.exporters import Exporter
+from modelscope.models import Model
+from modelscope.utils.constant import Tasks
+from modelscope.utils.test_utils import test_level
+
+
+class TestExportStableDiffusion(unittest.TestCase):
+
+    def setUp(self):
+        print(('Testing %s.%s' % (type(self).__name__, self._testMethodName)))
+        self.tmp_dir = tempfile.TemporaryDirectory().name
+        if not os.path.exists(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
+        self.model_id = 'AI-ModelScope/stable-diffusion-v1-5'
+
+    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    def test_export_stable_diffusion(self):
+        model = Model.from_pretrained(self.model_id)
+        print(Exporter.from_model(model).export_onnx(output_dir=self.tmp_dir))
+
+
+if __name__ == '__main__':
+    unittest.main()

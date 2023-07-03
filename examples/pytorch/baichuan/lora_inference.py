2f7edc5bed14b83e7a291c1dee733f80c0464330
hemu <hemu.zp@alibaba-inc.com>
2023-07-03 21:43:55 +0800

    fix linttest

--- examples/pytorch/baichuan/lora_inference.py
+++ examples/pytorch/baichuan/lora_inference.py
@@ -1,18 +1,18 @@
 import os.path as osp
+
 import torch
+
 from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
-from modelscope.swift.lora import LoRAConfig
 from modelscope.swift import Swift
+from modelscope.swift.lora import LoRAConfig
+from modelscope.utils.constant import Tasks
 
 # 使用源模型 model_id 初始化 pipeline
 model_id = 'baichuan-inc/baichuan-7B'
-pipe = pipeline(task=Tasks.text_generation, model=model_id, model_revision='v1.0.2')
+pipe = pipeline(
+    task=Tasks.text_generation, model=model_id, model_revision='v1.0.2')
 # lora 配置，replace_modules，rank，alpha 需与训练参数相同
-lora_config = LoRAConfig(
-    replace_modules=['pack'],
-    rank=32,
-    lora_alpha=32)
+lora_config = LoRAConfig(replace_modules=['pack'], rank=32, lora_alpha=32)
 # 转 bf16，需与训练精度相同
 model = pipe.model.bfloat16()
 # model 转 lora
@@ -26,4 +26,3 @@ pipe.model = model
 # 使用 pipeline 推理
 result_zh = pipe('今天天气是真的')
 print(result_zh)
-

f0257dd5013158311269bb633ee1da74d2248434
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-07-05 00:08:38 +0800

    fix hackathon issues

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.7.0'
+__version__ = '1.7.1'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
 __release_datetime__ = '2023-07-03 00:00:00'

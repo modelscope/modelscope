788695381a180efb533fcc8954c97e088e8a2b65
lylalala <liuyang070424@163.com>
2023-07-23 01:31:59 +0000

    update llama2 chat testcase

--- tests/models/test_llama2.py
+++ tests/models/test_llama2.py
@@ -1,9 +1,12 @@
 import unittest
 
-from modelscope.utils.test_utils import test_level
 import torch
-from modelscope import snapshot_download, Model
+
+from modelscope import Model, snapshot_download
 from modelscope.models.nlp.llama2 import Llama2Tokenizer
+from modelscope.utils.test_utils import test_level
+
+print('lala')
 
 
 class Llama2Test(unittest.TestCase):
@@ -16,8 +19,7 @@ class Llama2Test(unittest.TestCase):
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
     def test_chat(self):
         model_dir = snapshot_download(
-            self.model_name,
-            ignore_file_pattern=[r'\w+\.safetensors'])
+            self.model_name, ignore_file_pattern=[r'\w+\.safetensors'])
         model = Model.from_pretrained(
             model_dir, device_map='auto', torch_dtype=torch.float16)
         tokenizer = Llama2Tokenizer.from_pretrained(model_dir)
@@ -30,6 +32,7 @@ class Llama2Test(unittest.TestCase):
         result = model.chat(input=inputs, tokenizer=tokenizer)
         assert isinstance(result['history'], list)
         assert result['history'][0][0] == self.utterance
+
 
 if __name__ == '__main__':
     unittest.main()

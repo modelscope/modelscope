3c89b24e11ee952e09dbd8487e621cfd8401b763
lylalala <liuyang070424@163.com>
2023-07-23 01:21:11 +0000

    update llama2 chat testcase

--- tests/models/test_llama2.py
+++ tests/models/test_llama2.py
@@ -6,17 +6,17 @@ from modelscope.utils.test_utils import test_level
 class Llama2Test(unittest.TestCase):
 
     def setUp(self) -> None:
+        self.model_name = 'modelscope/Llama-2-7b-chat-ms'
         self.system = 'you are a helpful assistant!'
         self.utterance = 'hello'
 
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
-    def chat(self):
+    def test_chat(self):
         import torch
         from modelscope import snapshot_download, Model
         from modelscope.models.nlp.llama2 import Llama2Tokenizer
         model_dir = snapshot_download(
-            'modelscope/Llama-2-7b-chat-ms',
-            revision='v1.0.2',
+            self.model_name,
             ignore_file_pattern=[r'\w+\.safetensors'])
         model = Model.from_pretrained(
             model_dir, device_map='auto', torch_dtype=torch.float16)
@@ -29,4 +29,4 @@ class Llama2Test(unittest.TestCase):
         }
         result = model.chat(input=inputs, tokenizer=tokenizer)
         assert isinstance(result['history'], list)
-        assert result['history'][0] == self.utterance
+        assert result['history'][0][0] == self.utterance

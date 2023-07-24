9059a81e414a824721d73d1ed79294e283da5b00
lylalala <liuyang070424@163.com>
2023-07-24 06:47:48 +0000

    update unittest and support max_length in multi-turn dialogue

--- modelscope/models/nlp/llama2/text_generation.py
+++ modelscope/models/nlp/llama2/text_generation.py
@@ -33,16 +33,40 @@ from ... import MODELS
 from .backbone import Llama2Model, LlamaPreTrainedModel
 
 
-def get_chat_prompt(system: str, utterance: str,
-                    history: List[Tuple[str, str]]) -> str:
-    chat_prompt = [f'[INST] <<SYS>>\n{system}\n<</SYS>>\n\n']
-    for user, bot in history:
+def get_chat_prompt(system: str, text: str, history: List[Tuple[str, str]],
+                    max_length: int, tokenizer):
+    system_prompt = f'<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n'
+    system_ids = tokenizer(system_prompt, return_tensors='pt').input_ids
+
+    text_prompt = f'{text.strip()} [/INST]'
+    text_ids = tokenizer(text_prompt, return_tensors='pt').input_ids
+
+    prompt_length = system_ids.shape[-1] + text_ids.shape[-1]
+    if prompt_length > max_length:
+        raise RuntimeError(
+            f'prepend prompt length {prompt_length} is bigger than max_length {max_length}'
+        )
+
+    history_prompt = ''
+    history_ids_list = []
+    # traverse history in reverse order
+    for user, bot in history[::-1]:
         assert isinstance(user, str)
         assert isinstance(bot, str)
-        chat_prompt.append(
-            f'{user.strip()} [/INST] {bot.strip()} </s><s> [INST] ')
-    chat_prompt.append(f'{utterance.strip()} [/INST]')
-    return ''.join(chat_prompt)
+        round_prompt = f'{user.strip()} [/INST] {bot.strip()} </s><s>[INST] '
+        round_ids = tokenizer(round_prompt, return_tensors='pt').input_ids
+        if prompt_length + round_ids.shape[-1] > max_length:
+            # excess history should not be appended to the prompt
+            break
+        else:
+            history_prompt = round_prompt + history_prompt
+            history_ids_list = [round_ids] + history_ids_list
+            prompt_length += round_ids.shape[-1]
+
+    prompt_list = [system_prompt, history_prompt, text_prompt]
+    prompt_ids_list = [system_ids] + history_ids_list + [text_ids]
+
+    return ''.join(prompt_list), torch.cat(prompt_ids_list, dim=1)
 
 
 # This file is mainly copied from the llama code of transformers
@@ -203,12 +227,11 @@ class Llama2ForTextGeneration(LlamaPreTrainedModel):
     def chat(self, input: Dict, tokenizer) -> Dict:
         import copy
         gen_kwargs = copy.copy(input)
-        if 'utterance' not in input:
-            utterance: str = input['text']
-            gen_kwargs.pop('text')
+        if 'text' not in input:
+            text: str = ''
         else:
-            utterance: str = input['utterance']
-            gen_kwargs.pop('utterance')
+            text: str = input['text']
+            gen_kwargs.pop('text')
 
         if 'system' not in input:
             system: str = ''
@@ -223,18 +246,23 @@ class Llama2ForTextGeneration(LlamaPreTrainedModel):
             gen_kwargs.pop('history')
 
         if 'max_length' not in gen_kwargs:
-            gen_kwargs['max_length'] = 2048
+            gen_kwargs['max_length'] = 4096
 
-        prompt = get_chat_prompt(
-            system=system, utterance=utterance, history=history)
-        inputs = tokenizer(prompt, return_tensors='pt')
-        input_ids = inputs.input_ids.to(self.device)
+        prompt, prompt_ids = get_chat_prompt(
+            system=system,
+            text=text,
+            history=history,
+            max_length=gen_kwargs['max_length'],
+            tokenizer=tokenizer)
+        input_ids = prompt_ids.to(self.device)
         generate_ids = self.generate(input_ids, **gen_kwargs)
+        # remove input tokens
+        generate_ids = generate_ids[:, input_ids.shape[1]:]
         response = tokenizer.batch_decode(
             generate_ids,
             skip_special_tokens=True,
             clean_up_tokenization_spaces=False)[0]
-        response = response[len(prompt):].strip()
-        history.append((utterance, response))
+        response = response.strip()
+        history.append((text, response))
 
         return {OutputKeys.RESPONSE: response, OutputKeys.HISTORY: history}
--- tests/models/test_llama2.py
+++ tests/models/test_llama2.py
@@ -12,7 +12,9 @@ class Llama2Test(unittest.TestCase):
     def setUp(self) -> None:
         self.model_name = 'modelscope/Llama-2-7b-chat-ms'
         self.system = 'you are a helpful assistant!'
-        self.utterance = 'hello'
+        self.text_first_round = 'hello'
+        self.text_second_round = 'do you know peking university?'
+        self.text_third_round = 'where is it?'
 
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
     def test_chat(self):
@@ -23,13 +25,34 @@ class Llama2Test(unittest.TestCase):
         tokenizer = Llama2Tokenizer.from_pretrained(model_dir)
 
         inputs = {
-            'utterance': self.utterance,
+            'text': self.text_first_round,
             'history': [],
             'system': self.system
         }
         result = model.chat(input=inputs, tokenizer=tokenizer)
-        assert isinstance(result['history'], list)
-        assert result['history'][0][0] == self.utterance
+        self.assertIsInstance(result['history'], list)
+        self.assertEqual(len(result['history']), 1)
+        self.assertEqual(result['history'][0][0], self.text_first_round)
+
+        inputs = {
+            'text': self.text_second_round,
+            'history': result['history'],
+            'system': self.system
+        }
+        result = model.chat(input=inputs, tokenizer=tokenizer)
+        self.assertIsInstance(result['history'], list)
+        self.assertEqual(len(result['history']), 2)
+        self.assertEqual(result['history'][1][0], self.text_second_round)
+
+        inputs = {
+            'text': self.text_third_round,
+            'history': result['history'],
+            'system': self.system
+        }
+        result = model.chat(input=inputs, tokenizer=tokenizer)
+        self.assertIsInstance(result['history'], list)
+        self.assertEqual(len(result['history']), 3)
+        self.assertEqual(result['history'][2][0], self.text_third_round)
 
 
 if __name__ == '__main__':

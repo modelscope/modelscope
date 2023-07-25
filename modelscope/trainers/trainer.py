b2c2e41d3039d70acad342f92e81e4531c9eafee
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-07-25 16:03:32 +0800

    Fix bug

--- modelscope/trainers/trainer.py
+++ modelscope/trainers/trainer.py
@@ -258,7 +258,7 @@ class EpochBasedTrainer(BaseTrainer):
             # If not working in parallel scenario, put model to device as a default logic.
             device_name = self.device if self.device is not None else 'gpu'
             self.device = create_device(device_name)
-            if self.device.type == 'cuda' and is_on_same_device(model):
+            if self.device.type == 'cuda' and is_on_same_device(self.model):
                 self.model.to(self.device)
 
         self.print_cfg()

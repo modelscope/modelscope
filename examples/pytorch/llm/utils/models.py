9f055dc9be930756cacabaf9f94cdafeed6724ad
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-07-26 15:43:05 +0800

    update

--- examples/pytorch/llm/utils/models.py
+++ examples/pytorch/llm/utils/models.py
@@ -22,10 +22,10 @@ def _add_special_token(tokenizer):
                 f'pad_token_id: {tokenizer.pad_token_id}')
 
 
-def get_baichuan_model_tokenizer(model_dir: str,
-                                 load_model: bool = True,
-                                 add_special_token: bool = True,
-                                 torch_dtype: Dtype = torch.float16):
+def get_model_tokenizer_default(model_dir: str,
+                                load_model: bool = True,
+                                add_special_token: bool = True,
+                                torch_dtype: Dtype = torch.float16):
     """load from an independent repository"""
     model_config = AutoConfig.from_pretrained(
         model_dir, trust_remote_code=True)
@@ -47,7 +47,7 @@ def get_baichuan_model_tokenizer(model_dir: str,
     return model, tokenizer
 
 
-def get_chatglm2_model_tokenizer(model_dir: str,
+def get_model_tokenizer_chatglm2(model_dir: str,
                                  load_model: bool = True,
                                  add_special_token: bool = True,
                                  torch_dtype: Dtype = torch.float16):
@@ -71,26 +71,6 @@ def get_chatglm2_model_tokenizer(model_dir: str,
     return model, tokenizer
 
 
-def get_llama2_model_tokenizer(model_dir: str,
-                               load_model: bool = True,
-                               add_special_token: bool = True,
-                               torch_dtype: Dtype = torch.float16):
-    model_config = AutoConfig.from_pretrained(model_dir)
-    model_config.torch_dtype = torch_dtype
-    logger.info(model_config)
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
-    model = None
-    if load_model:
-        model = AutoModelForCausalLM.from_pretrained(
-            model_dir,
-            config=model_config,
-            device_map='auto',
-            torch_dtype=torch_dtype)
-    if add_special_token:
-        _add_special_token(tokenizer)
-    return model, tokenizer
-
-
 class LoRATM(NamedTuple):
     # default lora target modules
     baichuan = ['W_pack']
@@ -103,39 +83,34 @@ MODEL_MAPPER = {
     'baichuan-7b': {
         'model_id': 'baichuan-inc/baichuan-7B',
         'revision': 'v1.0.7',
-        'get_function': get_baichuan_model_tokenizer,
         'lora_TM': LoRATM.baichuan
     },
     'baichuan-13b': {
         'model_id': 'baichuan-inc/Baichuan-13B-Base',
         'revision': 'v1.0.3',
-        'get_function': get_baichuan_model_tokenizer,
         'lora_TM': LoRATM.baichuan
     },
     'chatglm2': {
         'model_id': 'ZhipuAI/chatglm2-6b',
         'revision': 'v1.0.6',
-        'get_function': get_chatglm2_model_tokenizer,
+        'get_function': get_model_tokenizer_chatglm2,
         'lora_TM': LoRATM.chatglm2
     },
     'llama2-7b': {
         'model_id': 'modelscope/Llama-2-7b-ms',
         'revision': 'v1.0.2',
         'ignore_file_pattern': [r'.+\.bin$'],
-        'get_function': get_llama2_model_tokenizer,
         'lora_TM': LoRATM.llama2
     },
     'llama2-13b': {
         'model_id': 'modelscope/Llama-2-13b-ms',
         'revision': 'v1.0.2',
         'ignore_file_pattern': [r'.+\.bin$'],
-        'get_function': get_llama2_model_tokenizer,
         'lora_TM': LoRATM.llama2
     },
     'openbuddy-llama2-13b': {
         'model_id': 'OpenBuddy/openbuddy-llama2-13b-v8.1-fp16',
         'revision': 'master',
-        'get_function': get_llama2_model_tokenizer,
         'lora_TM': LoRATM.llama2
     }
 }
@@ -150,7 +125,7 @@ def get_model_tokenizer(model_type: str,
         raise ValueError(f'model_type: {model_type}')
     model_id = data['model_id']
     revision = data['revision']
-    get_function = data['get_function']
+    get_function = data.get('get_function', get_model_tokenizer_default)
     ignore_file_pattern = data.get('ignore_file_pattern', [])
     model_dir = snapshot_download(
         model_id, revision, ignore_file_pattern=ignore_file_pattern)

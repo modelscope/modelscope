99aa7079952afa677b033f35fdb5df2c666826ee
XDUWQ <1300964705@qq.com>
2023-07-26 16:35:40 +0800

    fix bugs

--- examples/pytorch/stable_diffusion/custom/run_train_custom.sh
+++ examples/pytorch/stable_diffusion/custom/run_train_custom.sh
@@ -1,5 +1,5 @@
 PYTHONPATH=. torchrun examples/pytorch/stable_diffusion/custom/finetune_stable_diffusion_custom.py \
-    --model 'AI-ModelScope/stable-diffusion-v1-5' \
+    --model 'AI-ModelScope/stable-diffusion-v2-1' \
     --model_revision 'v1.0.9' \
     --class_prompt "dog" \
     --instance_prompt="photo of a <new1> dog" \
@@ -7,7 +7,6 @@ PYTHONPATH=. torchrun examples/pytorch/stable_diffusion/custom/finetune_stable_d
     --class_data_dir './tmp/class_data' \
     --train_dataset_name 'buptwq/lora-stable-diffusion-finetune-dog' \
     --max_epochs 250 \
-    --concepts_list '/mnt/user/E-yijing.wq-401594/github/custom_diffusion/modelscope/json/concept_list.json' \
     --modifier_token "<new1>+<new2>" \
     --num_class_images=200 \
     --save_ckpt_strategy 'by_epoch' \

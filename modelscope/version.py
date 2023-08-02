2e9401c0b0893f1439bea0d2dadc9bec7f1d2fc7
suluyan.sly <suluyan.sly@alibaba-inc.com>
2023-08-02 10:18:24 +0800

    v1.8

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.8.0rc0'
+__version__ = '1.8.0'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2023-07-31 23:59:59'
+__release_datetime__ = '2023-08-02 23:59:59'

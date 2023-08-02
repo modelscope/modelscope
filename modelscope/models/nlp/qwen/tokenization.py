bd2f70a6eb68eba988411d48a5e5609715f6b983
lukeming.lkm <lukeming.lkm@alibaba-inc.com>
2023-08-02 14:05:13 +0800

    add quantization in qwen pipelines and relevant unittests Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/13499600
    * add quant features
    
    * resolve import
    
    * resolve format
    
    * fix save vocab

--- modelscope/models/nlp/qwen/tokenization.py
+++ modelscope/models/nlp/qwen/tokenization.py
@@ -150,19 +150,19 @@ class QWenTokenizer(PreTrainedTokenizer):
                 format(len(ids), self.max_len))
         return ids
 
-    def save_vocabulary(self,
-                        save_directory: str,
-                        filename_prefix: Optional[str] = None) -> Tuple[str]:
+    def save_vocabulary(self, save_directory: str) -> Tuple[str]:
         """
         Save only the vocabulary of the tokenizer (vocabulary + added tokens).
 
         Returns:
             `Tuple(str)`: Paths to the files saved.
         """
-        file_path = os.path.join(save_directory, filename_prefix)
-        with open(file_path, 'w') as f:
-            json.dump(f, self.mergeable_ranks)
-        return file_path
+        file_path = os.path.join(save_directory, 'qwen.tiktoken')
+        with open(file_path, 'w', encoding='utf8') as w:
+            for k, v in self.mergeable_ranks.items():
+                line = base64.b64encode(k).decode('utf8') + ' ' + str(v) + '\n'
+                w.write(line)
+        return (file_path, )
 
     def tokenize(self, text: str, **kwargs) -> List[str]:
         """

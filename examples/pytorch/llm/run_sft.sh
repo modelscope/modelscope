344d2a2e5b4478009898b45a1ff4fa0e5f9b9562
xiangpeng.wxp <xiangpeng.wxp@alibaba-inc.com>
2023-08-14 15:37:42 +0800

    [to #42322933] add truct remote code for polylm Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/13628684 * add truct remote code for polylm
    * [to #42322933] add truct remote code for polylm
    
    * [to #42322933] add truct remote code for polylm

--- examples/pytorch/llm/run_sft.sh
+++ examples/pytorch/llm/run_sft.sh
@@ -2,5 +2,5 @@ CUDA_VISIBLE_DEVICES=0,1 \
 python llm_sft.py \
     --model_type polylm-13b \
     --output_dir runs \
-    --dataset alpaca-en,alpaca-zh \
+    --dataset alpaca-en,alpaca-zh,alpaca-multi \
     --dataset_sample 20000
--- examples/pytorch/llm/utils/dataset.py
+++ examples/pytorch/llm/utils/dataset.py
@@ -25,6 +25,28 @@ def _processing_alpaca(dataset: HfDataset) -> HfDataset:
     return dataset
 
 
+def _processing_multi_alpaca(datasets: [HfDataset, List]) -> HfDataset:
+    output = []
+    res = []
+
+    if not isinstance(datasets, List):
+        datasets = [datasets]
+    for dataset in datasets:
+        instruction = dataset['instruction']
+        input_ = dataset['input']
+        output_ = dataset['output']
+        for inst, inp, opt in zip(instruction, input_, output_):
+            if inp is not None and inp != '':
+                if inp.startswith('输入：'):
+                    inp = inp[3:]
+                inst = f'{inst}\n{inp}'
+            if opt is not None and opt != '':
+                res.append(inst)
+                output.append(opt)
+    dataset = HfDataset.from_dict({'instruction': res, 'output': output})
+    return dataset
+
+
 def get_alpaca_en_dataset() -> HfDataset:
     dataset_en: HfDataset = MsDataset.load(
         'AI-ModelScope/alpaca-gpt4-data-en', split='train').to_hf_dataset()
@@ -36,6 +58,19 @@ def get_alpaca_zh_dataset() -> HfDataset:
     dataset_zh: HfDataset = MsDataset.load(
         'AI-ModelScope/alpaca-gpt4-data-zh', split='train').to_hf_dataset()
     return _processing_alpaca(dataset_zh)
+
+
+def get_multi_alpaca_dataset() -> HfDataset:
+    dataset_multi = []
+    for subset_name in [
+            'ar', 'de', 'es', 'fr', 'id', 'ja', 'ko', 'pt', 'ru', 'th', 'vi'
+    ]:
+        dataset_sub: HfDataset = MsDataset.load(
+            'damo/nlp_polylm_multialpaca_sft',
+            subset_name=subset_name,
+            split='train').to_hf_dataset()
+        dataset_multi.append(dataset_sub)
+    return _processing_multi_alpaca(dataset_multi)
 
 
 def get_seed(random_state: RandomState) -> int:
@@ -59,6 +94,7 @@ def process_dataset(dataset: HfDataset, dataset_test_size: float,
 DATASET_MAPPER = {
     'alpaca-en': get_alpaca_en_dataset,
     'alpaca-zh': get_alpaca_zh_dataset,
+    'alpaca-multi': get_multi_alpaca_dataset,
 }
 
 

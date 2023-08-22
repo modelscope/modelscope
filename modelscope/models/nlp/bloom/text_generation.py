2e151eb03bb874ca2c7c04cef49503ea98a569d7
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-08-22 15:15:15 +0800

    pre-commit passed

--- modelscope/models/nlp/bloom/text_generation.py
+++ modelscope/models/nlp/bloom/text_generation.py
@@ -1,12 +1,14 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
-from modelscope.models import MODELS
 from transformers import BloomConfig
 from transformers import BloomForCausalLM as BloomForCausalLMTransform
 
+from modelscope.metainfo import Models
+from modelscope.models import MODELS
 from modelscope.utils.constant import Tasks
 
 
-@MODELS.register_module(group_key=Tasks.text_generation, module_name=Models.bloom)
+@MODELS.register_module(
+    group_key=Tasks.text_generation, module_name=Models.bloom)
 class BloomForTextGeneration(BloomForCausalLMTransform):
 
     def __init__(self, **kwargs):

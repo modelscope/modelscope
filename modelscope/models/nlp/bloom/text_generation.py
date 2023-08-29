e915e4fb3f77f249911081bfc3308b73b1d9485a
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-08-29 11:30:14 +0800

    fix bloom

--- modelscope/models/nlp/bloom/text_generation.py
+++ modelscope/models/nlp/bloom/text_generation.py
@@ -1,16 +1,14 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
-from transformers import BloomConfig
 from transformers import BloomForCausalLM as BloomForCausalLMTransform
 
 from modelscope.metainfo import Models
 from modelscope.models import MODELS
 from modelscope.utils.constant import Tasks
+from .backbone import MsModelMixin, TorchModel
 
 
 @MODELS.register_module(
     group_key=Tasks.text_generation, module_name=Models.bloom)
-class BloomForTextGeneration(BloomForCausalLMTransform):
+class BloomForTextGeneration(MsModelMixin, BloomForCausalLMTransform, TorchModel):
 
-    def __init__(self, **kwargs):
-        config = BloomConfig(**kwargs)
-        super().__init__(config)
+    pass

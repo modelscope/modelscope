d22a785136d5acef78955d7154641f4f7f8a8850
lingcai.wl <lingcai.wl@alibaba-inc.com>
2023-09-27 19:42:54 +0800

    [to #51336898] fix minor problems in deploying Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/14190317

--- modelscope/pipeline_inputs.py
+++ modelscope/pipeline_inputs.py
@@ -82,14 +82,28 @@ def check_input_type(input_type, input):
 
 
 TASK_INPUTS = {
+
+    # if task input is single var, value is  InputType
+    # if task input is a tuple,  value is tuple of InputType
+    # if task input is a dict, value is a dict of InputType, where key
+    # equals the one needed in pipeline input dict
+    # if task input is a list, value is a set of input format, in which
+    # each element corresponds to one input format as described above and
+    # must include a dict format.
+
+
+    Tasks.task_template: {
+        'image': InputType.IMAGE,
+        'text': InputType.TEXT
+    },
+    # ============ vision tasks ===================
+
     Tasks.image_text_retrieval: {
         InputKeys.IMAGE: InputType.IMAGE,
         InputKeys.TEXT: InputType.TEXT
     },
-    Tasks.general_recognition: {
-        InputKeys.IMAGE: InputType.IMAGE,
-        InputKeys.TEXT: InputType.TEXT
-    },
+    Tasks.general_recognition:
+    InputType.IMAGE,
     Tasks.video_depth_estimation: {
         InputKeys.IMAGE: InputType.IMAGE,
         InputKeys.TEXT: InputType.TEXT
@@ -110,20 +124,6 @@ TASK_INPUTS = {
     InputType.VIDEO,
 
 
-
-    Tasks.task_template: {
-        'image': InputType.IMAGE,
-        'text': InputType.TEXT
-    },
-    # if task input is single var, value is  InputType
-    # if task input is a tuple,  value is tuple of InputType
-    # if task input is a dict, value is a dict of InputType, where key
-    # equals the one needed in pipeline input dict
-    # if task input is a list, value is a set of input format, in which
-    # each element corresponds to one input format as described above and
-    # must include a dict format.
-
-    # ============ vision tasks ===================
     Tasks.ocr_detection:
     InputType.IMAGE,
     Tasks.ocr_recognition:

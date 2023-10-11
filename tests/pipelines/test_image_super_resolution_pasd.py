582c3a5415a3413d26aa1452793541563d2a6a60
baiguan.yt <baiguan.yt@alibaba-inc.com>
2023-10-11 09:59:19 +0800

    add pasd_v2 保真度更好
    Link: https://code.alibaba-inc.com/Ali-MaaS/MaaS-lib/codereview/14202282

--- tests/pipelines/test_image_super_resolution_pasd.py
+++ tests/pipelines/test_image_super_resolution_pasd.py
@@ -15,13 +15,14 @@ class ImageSuperResolutionPASDTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.model_id = 'damo/PASD_image_super_resolutions'
+        self.model_v2_id = 'damo/PASD_v2_image_super_resolutions'
         self.img = 'data/test/images/dogs.jpg'
         self.input = {
             'image': self.img,
             'prompt': '',
             'upscale': 1,
-            'fidelity_scale_fg': 1.5,
-            'fidelity_scale_bg': 0.7
+            'fidelity_scale_fg': 1.0,
+            'fidelity_scale_bg': 1.0
         }
         self.task = Tasks.image_super_resolution_pasd
 
@@ -35,6 +36,13 @@ class ImageSuperResolutionPASDTest(unittest.TestCase):
     def test_run_modelhub(self):
         super_resolution = pipeline(
             Tasks.image_super_resolution_pasd, model=self.model_id)
+
+        self.pipeline_inference(super_resolution, self.input)
+
+    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    def test_run_modelhub_v2(self):
+        super_resolution = pipeline(
+            Tasks.image_super_resolution_pasd, model=self.model_v2_id)
 
         self.pipeline_inference(super_resolution, self.input)
 

864fb4887251ce368c38fbf6fe254e2e0b30fb6d
suluyana <110878454+suluyana@users.noreply.github.com>
2023-12-04 16:51:35 +0800

    fix vllm gen cfg & set default to llm-ppl (#658)
    * fix vllm gen cfg & set default to llm-ppl
    
    * fix code style
    
    * reset llm_framework default value
    
    * fix pre-commit
    
    ---------
    
    Co-authored-by: suluyan.sly <suluyan.sly@alibaba-inc.com>

--- modelscope/pipelines/nlp/llm_pipeline.py
+++ modelscope/pipelines/nlp/llm_pipeline.py
@@ -104,12 +104,20 @@ class LLMPipeline(Pipeline):
 
         if isinstance(model, str) and is_official_hub_path(model):
             logger.info(f'initiate model from location {model}.')
-            if self.llm_framework is not None:
+            if self.llm_framework:
                 model_dir = model if os.path.exists(
                     model) else snapshot_download(model)
-                return self._wrap_infer_framework(model_dir,
-                                                  self.llm_framework)
-            elif is_model(model):
+                try:
+                    model = self._wrap_infer_framework(model_dir,
+                                                       self.llm_framework)
+                    logger.info(f'initiate model with {framework}.')
+                    return model
+                except Exception as e:
+                    self.llm_framework = None
+                    logger.warning(
+                        f'Cannot using llm_framework with {model}, '
+                        f'ignoring llm_framework={self.llm_framework} : {e}')
+            if is_model(model):
                 return Model.from_pretrained(
                     model,
                     invoked_by=Invoke.PIPELINE,

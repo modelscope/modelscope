dfb719ff567ec92fdbed8a1c8d76219ab6937944
muyuan.zq <gmall.zq.hitsz@gmail.com>
2023-12-20 12:35:49 +0000

    update unitest

--- tests/pipelines/test_image_to_3d.py
+++ tests/pipelines/test_image_to_3d.py
@@ -36,30 +36,6 @@ class ImageTo3DTest(unittest.TestCase):
             Tasks.image_to_3d, model=self.model_id, revision='v1.0.1')
         self.pipeline_inference(image_to_3d, self.input)
 
-    # @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
-    # def test_run_modelhub_default_model(self):
-    #     image_to_3d = pipeline(Tasks.image_to_3d, model=self.model_id)
-    #     self.pipeline_inference(image_to_3d, self.input)
-
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# from modelscope.pipelines import pipeline
-# from modelscope.utils.constant import Tasks
-# import numpy as np
-# from PIL import Image
-
-# image23D = pipeline(
-#             Tasks.image_to_3d,
-#             model='Damo_XR_Lab/Syncdreamer')
-
-# result = image23D('./basketball.png')
-
-# np_content = []
-# for idx,img in enumerate(result['MViews']):
-#     np_content.append(np.array(result['MViews'][idx]))
-
-# np_content = np.concatenate(np_content, axis=1)
-# Image.fromarray(np_content).save("./demo_output/concat.png")

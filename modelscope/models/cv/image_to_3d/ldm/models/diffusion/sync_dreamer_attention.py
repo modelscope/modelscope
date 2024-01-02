ec07a9919ad0023af44e96f9d3270c597e7f66a5
ly119399 <ly119399@alibaba-inc.com>
2024-01-03 00:05:04 +0800

    fix flake8

--- modelscope/models/cv/image_to_3d/ldm/models/diffusion/sync_dreamer_attention.py
+++ modelscope/models/cv/image_to_3d/ldm/models/diffusion/sync_dreamer_attention.py
@@ -1,17 +1,26 @@
 import torch
 import torch.nn as nn
 
-from modelscope.models.cv.image_to_3d.ldm.modules.attention import default, zero_module, checkpoint
-from modelscope.models.cv.image_to_3d.ldm.modules.diffusionmodules.openaimodel import UNetModel
-from modelscope.models.cv.image_to_3d.ldm.modules.diffusionmodules.util import timestep_embedding
+import modelscope.models.cv.image_to_3d.ldm.modules.attention as attention
+from modelscope.models.cv.image_to_3d.ldm.modules.diffusionmodules.openaimodel import \
+    UNetModel
+from modelscope.models.cv.image_to_3d.ldm.modules.diffusionmodules.util import \
+    timestep_embedding
+
 
 class DepthAttention(nn.Module):
-    def __init__(self, query_dim, context_dim, heads, dim_head, output_bias=True):
+
+    def __init__(self,
+                 query_dim,
+                 context_dim,
+                 heads,
+                 dim_head,
+                 output_bias=True):
         super().__init__()
         inner_dim = dim_head * heads
-        context_dim = default(context_dim, query_dim)
+        context_dim = attention.default(context_dim, query_dim)
 
-        self.scale = dim_head ** -0.5
+        self.scale = dim_head**-0.5
         self.heads = heads
         self.dim_head = dim_head
 
@@ -34,21 +43,27 @@ class DepthAttention(nn.Module):
         b, _, h, w = x.shape
         b, _, d, h, w = context.shape
 
-        q = self.to_q(x).reshape(b,hn,hd,h,w) # b,t,h,w
-        k = self.to_k(context).reshape(b,hn,hd,d,h,w) # b,t,d,h,w
-        v = self.to_v(context).reshape(b,hn,hd,d,h,w) # b,t,d,h,w
+        q = self.to_q(x).reshape(b, hn, hd, h, w)  # b,t,h,w
+        k = self.to_k(context).reshape(b, hn, hd, d, h, w)  # b,t,d,h,w
+        v = self.to_v(context).reshape(b, hn, hd, d, h, w)  # b,t,d,h,w
 
-        sim = torch.sum(q.unsqueeze(3) * k, 2) * self.scale # b,hn,d,h,w
+        sim = torch.sum(q.unsqueeze(3) * k, 2) * self.scale  # b,hn,d,h,w
         attn = sim.softmax(dim=2)
 
         # b,hn,hd,d,h,w * b,hn,1,d,h,w
-        out = torch.sum(v * attn.unsqueeze(2), 3) # b,hn,hd,h,w
-        out = out.reshape(b,hn*hd,h,w)
+        out = torch.sum(v * attn.unsqueeze(2), 3)  # b,hn,hd,h,w
+        out = out.reshape(b, hn * hd, h, w)
         return self.to_out(out)
 
 
 class DepthTransformer(nn.Module):
-    def __init__(self, dim, n_heads, d_head, context_dim=None, checkpoint=True):
+
+    def __init__(self,
+                 dim,
+                 n_heads,
+                 d_head,
+                 context_dim=None,
+                 checkpoint=True):
         super().__init__()
         inner_dim = n_heads * d_head
         self.proj_in = nn.Sequential(
@@ -57,23 +72,33 @@ class DepthTransformer(nn.Module):
             nn.SiLU(True),
         )
         self.proj_context = nn.Sequential(
-            nn.Conv3d(context_dim, context_dim, 1, 1, bias=False), # no bias
+            nn.Conv3d(context_dim, context_dim, 1, 1, bias=False),  # no bias
             nn.GroupNorm(8, context_dim),
-            nn.ReLU(True), # only relu, because we want input is 0, output is 0
+            nn.ReLU(
+                True),  # only relu, because we want input is 0, output is 0
         )
-        self.depth_attn = DepthAttention(query_dim=inner_dim, heads=n_heads, dim_head=d_head, context_dim=context_dim, output_bias=False)  # is a self-attention if not self.disable_self_attn
+        self.depth_attn = DepthAttention(
+            query_dim=inner_dim,
+            heads=n_heads,
+            dim_head=d_head,
+            context_dim=context_dim,
+            output_bias=False
+        )  # is a self-attention if not self.disable_self_attn
         self.proj_out = nn.Sequential(
             nn.GroupNorm(8, inner_dim),
             nn.ReLU(True),
             nn.Conv2d(inner_dim, inner_dim, 3, 1, 1, bias=False),
             nn.GroupNorm(8, inner_dim),
             nn.ReLU(True),
-            zero_module(nn.Conv2d(inner_dim, dim, 3, 1, 1, bias=False)),
+            attention.zero_module(
+                nn.Conv2d(inner_dim, dim, 3, 1, 1, bias=False)),
         )
-        self.checkpoint = checkpoint
+        self.checkpoint = attention.checkpoint
 
     def forward(self, x, context=None):
-        return checkpoint(self._forward, (x, context), self.parameters(), self.checkpoint)
+
+        return attention.checkpoint(self._forward, (x, context),
+                                    self.parameters(), self.checkpoint)  # noqa
 
     def _forward(self, x, context):
         x_in = x
@@ -85,38 +110,65 @@ class DepthTransformer(nn.Module):
 
 
 class DepthWiseAttention(UNetModel):
-    def __init__(self, volume_dims=(5,16,32,64), *args, **kwargs):
+
+    def __init__(self, volume_dims=(5, 16, 32, 64), *args, **kwargs):
         super().__init__(*args, **kwargs)
         # num_heads = 4
         model_channels = kwargs['model_channels']
         channel_mult = kwargs['channel_mult']
-        d0,d1,d2,d3 = volume_dims
+        d0, d1, d2, d3 = volume_dims
 
         # 4
-        ch = model_channels*channel_mult[2]
-        self.middle_conditions = DepthTransformer(ch, 4, d3 // 2, context_dim=d3)
+        ch = model_channels * channel_mult[2]
+        self.middle_conditions = DepthTransformer(
+            ch, 4, d3 // 2, context_dim=d3)
 
-        self.output_conditions=nn.ModuleList()
-        self.output_b2c = {3:0,4:1,5:2,6:3,7:4,8:5,9:6,10:7,11:8}
+        self.output_conditions = nn.ModuleList()
+        self.output_b2c = {
+            3: 0,
+            4: 1,
+            5: 2,
+            6: 3,
+            7: 4,
+            8: 5,
+            9: 6,
+            10: 7,
+            11: 8
+        }
         # 8
-        ch = model_channels*channel_mult[2]
-        self.output_conditions.append(DepthTransformer(ch, 4, d2 // 2, context_dim=d2)) # 0
-        self.output_conditions.append(DepthTransformer(ch, 4, d2 // 2, context_dim=d2)) # 1
+        ch = model_channels * channel_mult[2]
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d2 // 2, context_dim=d2))  # 0
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d2 // 2, context_dim=d2))  # 1
         # 16
-        self.output_conditions.append(DepthTransformer(ch, 4, d1 // 2, context_dim=d1)) # 2
-        ch = model_channels*channel_mult[1]
-        self.output_conditions.append(DepthTransformer(ch, 4, d1 // 2, context_dim=d1)) # 3
-        self.output_conditions.append(DepthTransformer(ch, 4, d1 // 2, context_dim=d1)) # 4
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d1 // 2, context_dim=d1))  # 2
+        ch = model_channels * channel_mult[1]
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d1 // 2, context_dim=d1))  # 3
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d1 // 2, context_dim=d1))  # 4
         # 32
-        self.output_conditions.append(DepthTransformer(ch, 4, d0 // 2, context_dim=d0)) # 5
-        ch = model_channels*channel_mult[0]
-        self.output_conditions.append(DepthTransformer(ch, 4, d0 // 2, context_dim=d0)) # 6
-        self.output_conditions.append(DepthTransformer(ch, 4, d0 // 2, context_dim=d0)) # 7
-        self.output_conditions.append(DepthTransformer(ch, 4, d0 // 2, context_dim=d0)) # 8
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d0 // 2, context_dim=d0))  # 5
+        ch = model_channels * channel_mult[0]
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d0 // 2, context_dim=d0))  # 6
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d0 // 2, context_dim=d0))  # 7
+        self.output_conditions.append(
+            DepthTransformer(ch, 4, d0 // 2, context_dim=d0))  # 8
 
-    def forward(self, x, timesteps=None, context=None, source_dict=None, **kwargs):
+    def forward(self,
+                x,
+                timesteps=None,
+                context=None,
+                source_dict=None,
+                **kwargs):
         hs = []
-        t_emb = timestep_embedding(timesteps, self.model_channels, repeat_only=False)
+        t_emb = timestep_embedding(
+            timesteps, self.model_channels, repeat_only=False)
         emb = self.time_embed(t_emb)
 
         h = x.type(self.dtype)
@@ -138,5 +190,6 @@ class DepthWiseAttention(UNetModel):
         return self.out(h)
 
     def get_trainable_parameters(self):
-        paras = [para for para in self.middle_conditions.parameters()] + [para for para in self.output_conditions.parameters()]
+        paras = [para for para in self.middle_conditions.parameters()
+                 ] + [para for para in self.output_conditions.parameters()]
         return paras
--- modelscope/models/cv/image_to_3d/ldm/modules/encoders/clip/clip.py
+++ modelscope/models/cv/image_to_3d/ldm/modules/encoders/clip/clip.py
@@ -2,15 +2,17 @@ import hashlib
 import os
 import urllib
 import warnings
-from typing import Any, Union, List
-from pkg_resources import packaging
+from typing import Any, List, Union
 
 import torch
 from PIL import Image
-from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+from pkg_resources import packaging
+from torchvision.transforms import (CenterCrop, Compose, Normalize, Resize,
+                                    ToTensor)
 from tqdm import tqdm
 
-from modelscope.models.cv.image_to_3d.ldm.modules.encoders.clip.model import build_model
+from modelscope.models.cv.image_to_3d.ldm.modules.encoders.clip.model import \
+    build_model
 
 try:
     from torchvision.transforms import InterpolationMode
@@ -18,23 +20,40 @@ try:
 except ImportError:
     BICUBIC = Image.BICUBIC
 
+if packaging.version.parse(
+        torch.__version__) < packaging.version.parse('1.7.1'):
+    warnings.warn('PyTorch version 1.7.1 or higher is recommended')
 
-if packaging.version.parse(torch.__version__) < packaging.version.parse("1.7.1"):
-    warnings.warn("PyTorch version 1.7.1 or higher is recommended")
-
-
-__all__ = ["available_models", "load"]
+__all__ = ['available_models', 'load']
 
 _MODELS = {
-    "RN50": "https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
-    "RN101": "https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
-    "RN50x4": "https://openaipublic.azureedge.net/clip/models/7e526bd135e493cef0776de27d5f42653e6b4c8bf9e0f653bb11773263205fdd/RN50x4.pt",
-    "RN50x16": "https://openaipublic.azureedge.net/clip/models/52378b407f34354e150460fe41077663dd5b39c54cd0bfd2b27167a4a06ec9aa/RN50x16.pt",
-    "RN50x64": "https://openaipublic.azureedge.net/clip/models/be1cfb55d75a9666199fb2206c106743da0f6468c9d327f3e0d0a543a9919d9c/RN50x64.pt",
-    "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
-    "ViT-B/16": "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
-    "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
-    "ViT-L/14@336px": "https://openaipublic.azureedge.net/clip/models/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",
+    'RN50':
+    'https://openaipublic.azureedge.net/clip/models/'
+    'afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt',
+    'RN101':
+    'https://openaipublic.azureedge.net/clip/models/'
+    '8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt',
+    'RN50x4':
+    'https://openaipublic.azureedge.net/clip/models/'
+    '7e526bd135e493cef0776de27d5f42653e6b4c8bf9e0f653bb11773263205fdd/RN50x4.pt',
+    'RN50x16':
+    'https://openaipublic.azureedge.net/clip/models/'
+    '52378b407f34354e150460fe41077663dd5b39c54cd0bfd2b27167a4a06ec9aa/RN50x16.pt',
+    'RN50x64':
+    'https://openaipublic.azureedge.net/clip/models/'
+    'be1cfb55d75a9666199fb2206c106743da0f6468c9d327f3e0d0a543a9919d9c/RN50x64.pt',
+    'ViT-B/32':
+    'https://openaipublic.azureedge.net/clip/models/'
+    '40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt',
+    'ViT-B/16':
+    'https://openaipublic.azureedge.net/clip/models/'
+    '5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt',
+    'ViT-L/14':
+    'https://openaipublic.azureedge.net/clip/models/'
+    'b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt',
+    'ViT-L/14@336px':
+    'https://openaipublic.azureedge.net/clip/models/'
+    '3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt',
 }
 
 
@@ -42,20 +61,30 @@ def _download(url: str, root: str):
     os.makedirs(root, exist_ok=True)
     filename = os.path.basename(url)
 
-    expected_sha256 = url.split("/")[-2]
+    expected_sha256 = url.split('/')[-2]
     download_target = os.path.join(root, filename)
 
     if os.path.exists(download_target) and not os.path.isfile(download_target):
-        raise RuntimeError(f"{download_target} exists and is not a regular file")
+        raise RuntimeError(
+            f'{download_target} exists and is not a regular file')
 
     if os.path.isfile(download_target):
-        if hashlib.sha256(open(download_target, "rb").read()).hexdigest() == expected_sha256:
+        if hashlib.sha256(open(download_target,
+                               'rb').read()).hexdigest() == expected_sha256:
             return download_target
         else:
-            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
+            warnings.warn(
+                f'{download_target} exists, but the SHA256 checksum does not match; re-downloading the file'
+            )
 
-    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
+    with urllib.request.urlopen(url) as source, open(download_target,
+                                                     'wb') as output:
+        with tqdm(
+                total=int(source.info().get('Content-Length')),
+                ncols=80,
+                unit='iB',
+                unit_scale=True,
+                unit_divisor=1024) as loop:
             while True:
                 buffer = source.read(8192)
                 if not buffer:
@@ -64,14 +93,17 @@ def _download(url: str, root: str):
                 output.write(buffer)
                 loop.update(len(buffer))
 
-    if hashlib.sha256(open(download_target, "rb").read()).hexdigest() != expected_sha256:
-        raise RuntimeError("Model has been downloaded but the SHA256 checksum does not not match")
+    if hashlib.sha256(open(download_target,
+                           'rb').read()).hexdigest() != expected_sha256:
+        raise RuntimeError(
+            'Model has been downloaded but the SHA256 checksum does not not match'
+        )
 
     return download_target
 
 
 def _convert_image_to_rgb(image):
-    return image.convert("RGB")
+    return image.convert('RGB')
 
 
 def _transform(n_px):
@@ -80,7 +112,8 @@ def _transform(n_px):
         CenterCrop(n_px),
         _convert_image_to_rgb,
         ToTensor(),
-        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        Normalize((0.48145466, 0.4578275, 0.40821073),
+                  (0.26862954, 0.26130258, 0.27577711)),
     ])
 
 
@@ -89,7 +122,11 @@ def available_models() -> List[str]:
     return list(_MODELS.keys())
 
 
-def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_available() else "cpu", jit: bool = False, download_root: str = None):
+def load(name: str,
+         device: Union[str, torch.device] = 'cuda'
+         if torch.cuda.is_available() else 'cpu',
+         jit: bool = False,
+         download_root: str = None):
     """Load a CLIP model
 
     Parameters
@@ -115,37 +152,47 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
         A torchvision transform that converts a PIL image into a tensor that the returned model can take as its input
     """
     if name in _MODELS:
-        model_path = _download(_MODELS[name], download_root or os.path.expanduser("~/.cache/clip"))
+        model_path = _download(
+            _MODELS[name], download_root
+            or os.path.expanduser('~/.cache/clip'))
     elif os.path.isfile(name):
         model_path = name
     else:
-        raise RuntimeError(f"Model {name} not found; available models = {available_models()}")
+        raise RuntimeError(
+            f'Model {name} not found; available models = {available_models()}')
 
     with open(model_path, 'rb') as opened_file:
         try:
             # loading JIT archive
-            model = torch.jit.load(opened_file, map_location=device if jit else "cpu").eval()
+            model = torch.jit.load(
+                opened_file, map_location=device if jit else 'cpu').eval()
             state_dict = None
         except RuntimeError:
             # loading saved state dict
             if jit:
-                warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
+                warnings.warn(
+                    f'File {model_path} is not a JIT archive. Loading as a state dict instead'
+                )
                 jit = False
-            state_dict = torch.load(opened_file, map_location="cpu")
+            state_dict = torch.load(opened_file, map_location='cpu')
 
     if not jit:
         model = build_model(state_dict or model.state_dict()).to(device)
-        if str(device) == "cpu":
+        if str(device) == 'cpu':
             model.float()
         return model, _transform(model.visual.input_resolution)
 
     # patch the device names
-    device_holder = torch.jit.trace(lambda: torch.ones([]).to(torch.device(device)), example_inputs=[])
-    device_node = [n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1]
+    device_holder = torch.jit.trace(
+        lambda: torch.ones([]).to(torch.device(device)), example_inputs=[])
+    device_node = [
+        n for n in device_holder.graph.findAllNodes('prim::Constant')
+        if 'Device' in repr(n)
+    ][-1]
 
     def _node_get(node: torch._C.Node, key: str):
         """Gets attributes of a node which is polymorphic over return type.
-        
+
         From https://github.com/pytorch/pytorch/pull/82628
         """
         sel = node.kindOf(key)
@@ -153,16 +200,17 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
 
     def patch_device(module):
         try:
-            graphs = [module.graph] if hasattr(module, "graph") else []
+            graphs = [module.graph] if hasattr(module, 'graph') else []
         except RuntimeError:
             graphs = []
 
-        if hasattr(module, "forward1"):
+        if hasattr(module, 'forward1'):
             graphs.append(module.forward1.graph)
 
         for graph in graphs:
-            for node in graph.findAllNodes("prim::Constant"):
-                if "value" in node.attributeNames() and str(_node_get(node, "value")).startswith("cuda"):
+            for node in graph.findAllNodes('prim::Constant'):
+                if 'value' in node.attributeNames() and str(
+                        _node_get(node, 'value')).startswith('cuda'):
                     node.copyAttributes(device_node)
 
     model.apply(patch_device)
@@ -170,25 +218,28 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
     patch_device(model.encode_text)
 
     # patch dtype to float32 on CPU
-    if str(device) == "cpu":
-        float_holder = torch.jit.trace(lambda: torch.ones([]).float(), example_inputs=[])
-        float_input = list(float_holder.graph.findNode("aten::to").inputs())[1]
+    if str(device) == 'cpu':
+        float_holder = torch.jit.trace(
+            lambda: torch.ones([]).float(), example_inputs=[])
+        float_input = list(float_holder.graph.findNode('aten::to').inputs())[1]
         float_node = float_input.node()
 
         def patch_float(module):
             try:
-                graphs = [module.graph] if hasattr(module, "graph") else []
+                graphs = [module.graph] if hasattr(module, 'graph') else []
             except RuntimeError:
                 graphs = []
 
-            if hasattr(module, "forward1"):
+            if hasattr(module, 'forward1'):
                 graphs.append(module.forward1.graph)
 
             for graph in graphs:
-                for node in graph.findAllNodes("aten::to"):
+                for node in graph.findAllNodes('aten::to'):
                     inputs = list(node.inputs())
-                    for i in [1, 2]:  # dtype can be the second or third argument to aten::to()
-                        if _node_get(inputs[i].node(), "value") == 5:
+                    for i in [
+                            1, 2
+                    ]:  # dtype can be the second or third argument to aten::to()
+                        if _node_get(inputs[i].node(), 'value') == 5:
                             inputs[i].node().copyAttributes(float_node)
 
         model.apply(patch_float)

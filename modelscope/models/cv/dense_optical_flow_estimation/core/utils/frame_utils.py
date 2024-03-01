4afa8e6f4e635098b566a3a2b046fa961820be92
kejie <kejie.qkj@alibaba-inc.com>
2024-03-01 21:32:37 +0800

    add frame_utils

--- modelscope/models/cv/dense_optical_flow_estimation/core/utils/frame_utils.py
+++ modelscope/models/cv/dense_optical_flow_estimation/core/utils/frame_utils.py
@@ -1,13 +1,15 @@
-import numpy as np
-from PIL import Image
-from os.path import *
 import re
+from os.path import *
 
 import cv2
+import numpy as np
+from PIL import Image
+
 cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
 
 TAG_CHAR = np.array([202021.25], np.float32)
+
 
 def readFlow(fn):
     """ Read .flo file in Middlebury format"""
@@ -25,10 +27,11 @@ def readFlow(fn):
             w = np.fromfile(f, np.int32, count=1)
             h = np.fromfile(f, np.int32, count=1)
             # print 'Reading %d x %d flo file\n' % (w, h)
-            data = np.fromfile(f, np.float32, count=2*int(w)*int(h))
+            data = np.fromfile(f, np.float32, count=2 * int(w) * int(h))
             # Reshape data into 3D array (columns, rows, bands)
             # The reshape here is for visualization, the original code is (w,h,2)
             return np.resize(data, (int(h), int(w), 2))
+
 
 def readPFM(file):
     file = open(file, 'rb')
@@ -54,11 +57,11 @@ def readPFM(file):
         raise Exception('Malformed PFM header.')
 
     scale = float(file.readline().rstrip())
-    if scale < 0: # little-endian
+    if scale < 0:  # little-endian
         endian = '<'
         scale = -scale
     else:
-        endian = '>' # big-endian
+        endian = '>'  # big-endian
 
     data = np.fromfile(file, endian + 'f')
     shape = (height, width, 3) if color else (height, width)
@@ -67,9 +70,10 @@ def readPFM(file):
     data = np.flipud(data)
     return data
 
-def writeFlow(filename,uv,v=None):
+
+def writeFlow(filename, uv, v=None):
     """ Write optical flow to file.
-    
+
     If v is None, uv is assumed to contain both u and v channels,
     stacked in depth.
     Original code by Deqing Sun, adapted from Daniel Scharstein.
@@ -77,34 +81,35 @@ def writeFlow(filename,uv,v=None):
     nBands = 2
 
     if v is None:
-        assert(uv.ndim == 3)
-        assert(uv.shape[2] == 2)
-        u = uv[:,:,0]
-        v = uv[:,:,1]
+        assert (uv.ndim == 3)
+        assert (uv.shape[2] == 2)
+        u = uv[:, :, 0]
+        v = uv[:, :, 1]
     else:
         u = uv
 
-    assert(u.shape == v.shape)
-    height,width = u.shape
-    f = open(filename,'wb')
+    assert (u.shape == v.shape)
+    height, width = u.shape
+    f = open(filename, 'wb')
     # write the header
     f.write(TAG_CHAR)
     np.array(width).astype(np.int32).tofile(f)
     np.array(height).astype(np.int32).tofile(f)
     # arrange into matrix form
-    tmp = np.zeros((height, width*nBands))
-    tmp[:,np.arange(width)*2] = u
-    tmp[:,np.arange(width)*2 + 1] = v
+    tmp = np.zeros((height, width * nBands))
+    tmp[:, np.arange(width) * 2] = u
+    tmp[:, np.arange(width) * 2 + 1] = v
     tmp.astype(np.float32).tofile(f)
     f.close()
 
 
 def readFlowKITTI(filename):
-    flow = cv2.imread(filename, cv2.IMREAD_ANYDEPTH|cv2.IMREAD_COLOR)
-    flow = flow[:,:,::-1].astype(np.float32)
+    flow = cv2.imread(filename, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
+    flow = flow[:, :, ::-1].astype(np.float32)
     flow, valid = flow[:, :, :2], flow[:, :, 2]
     flow = (flow - 2**15) / 64.0
     return flow, valid
+
 
 def readDispKITTI(filename):
     disp = cv2.imread(filename, cv2.IMREAD_ANYDEPTH) / 256.0
@@ -118,7 +123,7 @@ def writeFlowKITTI(filename, uv):
     valid = np.ones([uv.shape[0], uv.shape[1], 1])
     uv = np.concatenate([uv, valid], axis=-1).astype(np.uint16)
     cv2.imwrite(filename, uv[..., ::-1])
-    
+
 
 def read_gen(file_name, pil=False):
     ext = splitext(file_name)[-1]

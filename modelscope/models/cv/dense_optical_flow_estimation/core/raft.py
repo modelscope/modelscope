6561617889930dc56ecd15c2bfb3dd4303aaabbb
kejie <kejie.qkj@alibaba-inc.com>
2024-03-01 22:50:31 +0800

    test

--- modelscope/models/cv/dense_optical_flow_estimation/core/raft.py
+++ modelscope/models/cv/dense_optical_flow_estimation/core/raft.py
@@ -2,27 +2,34 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from modelscope.models.base.base_torch_model import TorchModel
+from modelscope.models.cv.dense_optical_flow_estimation.core.corr import (
+    AlternateCorrBlock, CorrBlock)
+from modelscope.models.cv.dense_optical_flow_estimation.core.extractor import (
+    BasicEncoder, SmallEncoder)
+from modelscope.models.cv.dense_optical_flow_estimation.core.update import (
+    BasicUpdateBlock, SmallUpdateBlock)
+from modelscope.models.cv.dense_optical_flow_estimation.core.utils.utils import (
+    bilinear_sampler, coords_grid, upflow8)
 
-from modelscope.models.cv.dense_optical_flow_estimation.core.update import BasicUpdateBlock, SmallUpdateBlock
-from modelscope.models.cv.dense_optical_flow_estimation.core.extractor import BasicEncoder, SmallEncoder
-from modelscope.models.cv.dense_optical_flow_estimation.core.corr import CorrBlock, AlternateCorrBlock
-from modelscope.models.cv.dense_optical_flow_estimation.core.utils.utils import bilinear_sampler, coords_grid, upflow8
+autocast = torch.cuda.amp.autocast
 
-try:
-    autocast = torch.cuda.amp.autocast
-except:
-    # dummy autocast for PyTorch < 1.6
-    class autocast:
-        def __init__(self, enabled):
-            pass
-        def __enter__(self):
-            pass
-        def __exit__(self, *args):
-            pass
+# try:
+#     autocast = torch.cuda.amp.autocast
+# except:
+#     # dummy autocast for PyTorch < 1.6
+#     class autocast:
+#         def __init__(self, enabled):
+#             pass
+#         def __enter__(self):
+#             pass
+#         def __exit__(self, *args):
+#             pass
 
 
 class RAFT(TorchModel):
+
     def __init__(self, args):
         super(RAFT, self).__init__()
         self.args = args
@@ -32,7 +39,7 @@ class RAFT(TorchModel):
             self.context_dim = cdim = 64
             args.corr_levels = 4
             args.corr_radius = 3
-        
+
         else:
             self.hidden_dim = hdim = 128
             self.context_dim = cdim = 128
@@ -47,13 +54,17 @@ class RAFT(TorchModel):
 
         # feature network, context network, and update block
         if args.small:
-            self.fnet = SmallEncoder(output_dim=128, norm_fn='instance', dropout=args.dropout)        
-            self.cnet = SmallEncoder(output_dim=hdim+cdim, norm_fn='none', dropout=args.dropout)
+            self.fnet = SmallEncoder(
+                output_dim=128, norm_fn='instance', dropout=args.dropout)
+            self.cnet = SmallEncoder(
+                output_dim=hdim + cdim, norm_fn='none', dropout=args.dropout)
             self.update_block = SmallUpdateBlock(self.args, hidden_dim=hdim)
 
         else:
-            self.fnet = BasicEncoder(output_dim=256, norm_fn='instance', dropout=args.dropout)        
-            self.cnet = BasicEncoder(output_dim=hdim+cdim, norm_fn='batch', dropout=args.dropout)
+            self.fnet = BasicEncoder(
+                output_dim=256, norm_fn='instance', dropout=args.dropout)
+            self.cnet = BasicEncoder(
+                output_dim=hdim + cdim, norm_fn='batch', dropout=args.dropout)
             self.update_block = BasicUpdateBlock(self.args, hidden_dim=hdim)
 
     def freeze_bn(self):
@@ -64,8 +75,8 @@ class RAFT(TorchModel):
     def initialize_flow(self, img):
         """ Flow is represented as difference between two coordinate grids flow = coords1 - coords0"""
         N, C, H, W = img.shape
-        coords0 = coords_grid(N, H//8, W//8, device=img.device)
-        coords1 = coords_grid(N, H//8, W//8, device=img.device)
+        coords0 = coords_grid(N, H // 8, W // 8, device=img.device)
+        coords1 = coords_grid(N, H // 8, W // 8, device=img.device)
 
         # optical flow computed as difference: flow = coords1 - coords0
         return coords0, coords1
@@ -76,15 +87,20 @@ class RAFT(TorchModel):
         mask = mask.view(N, 1, 9, 8, 8, H, W)
         mask = torch.softmax(mask, dim=2)
 
-        up_flow = F.unfold(8 * flow, [3,3], padding=1)
+        up_flow = F.unfold(8 * flow, [3, 3], padding=1)
         up_flow = up_flow.view(N, 2, 9, 1, 1, H, W)
 
         up_flow = torch.sum(mask * up_flow, dim=2)
         up_flow = up_flow.permute(0, 1, 4, 2, 5, 3)
-        return up_flow.reshape(N, 2, 8*H, 8*W)
+        return up_flow.reshape(N, 2, 8 * H, 8 * W)
 
-
-    def forward(self, image1, image2, iters=20, flow_init=None, upsample=True, test_mode=False):
+    def forward(self,
+                image1,
+                image2,
+                iters=20,
+                flow_init=None,
+                upsample=True,
+                test_mode=False):
         """ Estimate optical flow between pair of frames """
 
         image1 = 2 * (image1 / 255.0) - 1.0
@@ -98,12 +114,13 @@ class RAFT(TorchModel):
 
         # run the feature network
         with autocast(enabled=self.args.mixed_precision):
-            fmap1, fmap2 = self.fnet([image1, image2])        
-        
+            fmap1, fmap2 = self.fnet([image1, image2])
+
         fmap1 = fmap1.float()
         fmap2 = fmap2.float()
         if self.args.alternate_corr:
-            corr_fn = AlternateCorrBlock(fmap1, fmap2, radius=self.args.corr_radius)
+            corr_fn = AlternateCorrBlock(
+                fmap1, fmap2, radius=self.args.corr_radius)
         else:
             corr_fn = CorrBlock(fmap1, fmap2, radius=self.args.corr_radius)
 
@@ -122,11 +139,12 @@ class RAFT(TorchModel):
         flow_predictions = []
         for itr in range(iters):
             coords1 = coords1.detach()
-            corr = corr_fn(coords1) # index correlation volume
+            corr = corr_fn(coords1)  # index correlation volume
 
             flow = coords1 - coords0
             with autocast(enabled=self.args.mixed_precision):
-                net, up_mask, delta_flow = self.update_block(net, inp, corr, flow)
+                net, up_mask, delta_flow = self.update_block(
+                    net, inp, corr, flow)
 
             # F(t+1) = F(t) + \Delta(t)
             coords1 = coords1 + delta_flow
@@ -136,10 +154,10 @@ class RAFT(TorchModel):
                 flow_up = upflow8(coords1 - coords0)
             else:
                 flow_up = self.upsample_flow(coords1 - coords0, up_mask)
-            
+
             flow_predictions.append(flow_up)
 
         if test_mode:
             return coords1 - coords0, flow_up
-            
+
         return flow_predictions

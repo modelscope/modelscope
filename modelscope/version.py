7ba0b4d7cf7ba8f6a2a38e07ad2d63dc8cfe196b
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-03-22 17:43:41 +0800

    bump version

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.13.1'
+__version__ = '1.13.2'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2024-03-09 23:00:00'
+__release_datetime__ = '2024-03-24 23:00:00'

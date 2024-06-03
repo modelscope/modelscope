7b18f99d03df19c3e8f78d8fb81f4d68274836f5
mulin.lyh <mulin.lyh@taobao.com>
2024-06-03 10:09:07 +0800

    release to 06.05

--- modelscope/version.py
+++ modelscope/version.py
@@ -2,4 +2,4 @@
 __version__ = '1.15.0'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2099-09-06 00:00:00'
+__release_datetime__ = '2024-06-05 00:00:00'

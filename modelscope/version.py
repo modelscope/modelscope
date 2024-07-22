a5d68e908596e426b07ccb88aa021dca916f240e
liuyhwangyh <liuyhwangyh@163.com>
2024-07-22 19:37:29 +0800

    Update version.py

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.16.1'
+__version__ = '2.0.0'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2024-07-02 00:00:00'
+__release_datetime__ = '2099-09-06 00:00:00'

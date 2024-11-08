633d879fcfcd902533a63586eb3b3ef217d800d3
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-11-08 20:53:59 +0800

    update

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,7 @@ vllm_version=${4:-0.6.0}
 lmdeploy_version=${5:-0.6.1}
 autogptq_version=${6:-0.7.1}
 
-pip install --no-cache-dir -U autoawq
+pip install --no-cache-dir -U autoawq lmdeploy==$lmdeploy_version
 
 pip uninstall -y torch torchvision torchaudio
 
@@ -19,12 +19,6 @@ pip install --no-cache-dir tiktoken transformers_stream_generator bitsandbytes d
 # find on: https://github.com/Dao-AILab/flash-attention/releases
 cd /tmp && git clone https://github.com/Dao-AILab/flash-attention.git && cd flash-attention && python setup.py install && cd / && rm -fr /tmp/flash-attention && pip cache purge;
 
-pip install --no-cache-dir auto-gptq==$autogptq_version
+pip install --no-cache-dir triton auto-gptq==$autogptq_version vllm==$vllm_version -U && pip cache purge
 
 # pip uninstall -y torch-scatter && TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5;8.0;8.6;8.9;9.0" pip install --no-cache-dir -U torch-scatter
-
-pip install --no-cache-dir -U triton
-
-pip install --no-cache-dir vllm==$vllm_version -U
-
-pip install --no-cache-dir -U lmdeploy==$lmdeploy_version

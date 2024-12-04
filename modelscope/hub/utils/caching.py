7b86bf593384bf2d351ad802a6749e845f64a8d4
suluyana <suluyan_sly@163.com>
2024-12-04 20:34:49 +0800

    fix lint

--- modelscope/hub/utils/caching.py
+++ modelscope/hub/utils/caching.py
@@ -7,7 +7,9 @@ import tempfile
 from shutil import move, rmtree
 from typing import Dict
 
-from modelscope.hub.constants import FILE_HASH, MODELSCOPE_ENABLE_DEFAULT_HASH_VALIDATION
+from modelscope.hub.constants import (FILE_HASH,
+                                      MODELSCOPE_ENABLE_DEFAULT_HASH_VALIDATION
+                                      )
 from modelscope.hub.utils.utils import compute_hash
 from modelscope.utils.logger import get_logger
 

235dbf4c6354ed815f00ee6b6c67b7e9005aae11
mushenL <mushenl0412@gmail.com>
2024-12-10 10:59:38 +0800

    fix daily fail case

--- modelscope/models/cv/image_super_resolution_pasd_v2/unet_2d_blocks.py
+++ modelscope/models/cv/image_super_resolution_pasd_v2/unet_2d_blocks.py
@@ -20,12 +20,12 @@ from diffusers.models.activations import get_activation
 from diffusers.models.attention_processor import (Attention,
                                                   AttnAddedKVProcessor,
                                                   AttnAddedKVProcessor2_0)
-from diffusers.models.dual_transformer_2d import DualTransformer2DModel
+from diffusers.models.transformers.dual_transformer_2d import DualTransformer2DModel
 from diffusers.models.normalization import AdaLayerNorm
 from diffusers.models.resnet import (Downsample2D, FirDownsample2D,
                                      FirUpsample2D, KDownsample2D, KUpsample2D,
                                      ResnetBlock2D, Upsample2D)
-from diffusers.models.transformer_2d import Transformer2DModel
+from diffusers.models.transformers.transformer_2d import Transformer2DModel
 from diffusers.utils import is_torch_version, logging
 from einops import rearrange
 from torch import nn

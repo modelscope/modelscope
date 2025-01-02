49293a39d5838ec84017ffb83ad248d0f0b4584e
suluyana <suluyan_sly@163.com>
2025-01-02 10:16:28 +0800

    update ms-swift version

--- .dev_scripts/ci_container_test.sh
+++ .dev_scripts/ci_container_test.sh
@@ -32,7 +32,7 @@ if [ "$MODELSCOPE_SDK_DEBUG" == "True" ]; then
     pip install faiss-gpu
     pip install healpy
     pip install huggingface-hub==0.25.2
-    pip install ms-swift==3.0.0
+    pip install ms-swift>=3.0.1
     # test with install
     pip install .
 else

3475d7b6d7d05d5aebf3edace79849055d7a0a50
suluyana <suluyan_sly@163.com>
2025-01-15 14:35:14 +0800

    bump version 1.22.2

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.22.1'
+__version__ = '1.22.2'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
 __release_datetime__ = '2025-01-15 01:00:00'

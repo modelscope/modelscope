90707deec03316de7e64a6629b952950cbc141ba
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-02-05 18:38:10 +0800

    remove MODELSCOPE_SHOW_INDIVIDUAL_PROGRESS_THRESHOLD

--- modelscope/hub/snapshot_download.py
+++ modelscope/hub/snapshot_download.py
@@ -9,8 +9,6 @@ from pathlib import Path
 from typing import Dict, List, Optional, Union
 
 from modelscope.hub.api import HubApi, ModelScopeConfig
-from modelscope.hub.constants import \
-    MODELSCOPE_SHOW_INDIVIDUAL_PROGRESS_THRESHOLD
 from modelscope.hub.errors import InvalidParameter
 from modelscope.hub.file_download import (create_temporary_directory_and_cache,
                                           download_file, get_file_download_url)
@@ -500,9 +498,7 @@ def _download_file_lists(
             raise InvalidParameter(
                 f'Invalid repo type: {repo_type}, supported types: {REPO_TYPE_SUPPORT}'
             )
-        disable_tqdm = len(
-            filtered_repo_files
-        ) > MODELSCOPE_SHOW_INDIVIDUAL_PROGRESS_THRESHOLD  # noqa
+
         download_file(
             url,
             repo_file,
@@ -510,7 +506,7 @@ def _download_file_lists(
             cache,
             headers,
             cookies,
-            disable_tqdm=disable_tqdm,
+            disable_tqdm=False,
         )
 
     if len(filtered_repo_files) > 0:

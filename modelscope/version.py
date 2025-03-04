6c39f998e16381e81990120911ea9f58cf0fa2b4
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-03-04 20:02:30 +0800

    update version and cherry-pick refine commit info

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.23.1'
+__version__ = '1.23.2'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2025-02-21 00:00:00'
+__release_datetime__ = '2025-03-04 21:00:00'

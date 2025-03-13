68f73e59f6c81da592d660bd63cf049826fdfa91
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-03-14 01:30:50 +0800

    fix merge issue for get_endpoint

--- modelscope/hub/utils/utils.py
+++ modelscope/hub/utils/utils.py
@@ -121,11 +121,8 @@ def get_release_datetime():
     return rt
 
 
-def get_endpoint():
-    modelscope_domain = os.getenv(
-        MODELSCOPE_DOMAIN,
-        DEFAULT_MODELSCOPE_DOMAIN) or DEFAULT_MODELSCOPE_DOMAIN
-    return MODELSCOPE_URL_SCHEME + modelscope_domain
+def get_endpoint(cn_site=True):
+    return MODELSCOPE_URL_SCHEME + get_domain(cn_site)
 
 
 def compute_hash(file_path):

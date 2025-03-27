e4b8356af6e6bf015cb5d2de339d1eec06a2f0ec
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-03-27 22:57:32 +0800

    set 1.24.1

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.24.0'
+__version__ = '1.24.1'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2025-03-17 23:59:59'
+__release_datetime__ = '2025-03-28 23:59:59'

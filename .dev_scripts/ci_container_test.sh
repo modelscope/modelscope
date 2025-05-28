60a36e145d6f5aacfda4f1fbda18651197c1a5f5
suluyan <suluyan.sly@aliabab-inc.com>
2025-05-28 17:30:26 +0800

    set deps

--- .dev_scripts/ci_container_test.sh
+++ .dev_scripts/ci_container_test.sh
@@ -31,7 +31,7 @@ if [ "$MODELSCOPE_SDK_DEBUG" == "True" ]; then
     python -m spacy download en_core_web_sm
     pip install faiss-gpu
     pip install healpy
-    pip install huggingface-hub==0.25.2
+    pip install huggingface-hub
     pip install ms-swift>=3.0.1
     # test with install
     pip install .

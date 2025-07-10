7160f22b08155b1b197835dee71e0ab3b3abd35d
Koko <2024104299@ruc.edu.cn>
2025-07-10 10:36:16 +0800

    合并远程分支并修复冲突

--- modelscope/hub/__init__.py
+++ modelscope/hub/__init__.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
 from .api import HubApi
 from .callback import ProgressCallback
 
 __all__ = ['HubApi']
-=======
->>>>>>> 6c619155ac341ddfc2870a50bce7bb9abc4811a2

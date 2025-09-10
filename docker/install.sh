6d0ec7b8fcb52ed6f3a12fd546f8ed2744790719
wangxingjun778 <wangxingjun778@163.com>
2025-09-10 14:39:44 +0800

    fix install.sh for vllm version

--- docker/install.sh
+++ docker/install.sh
@@ -25,12 +25,14 @@ pip install --no-cache-dir flash_attn==$flashattn_version
 
 pip install --no-cache-dir triton auto-gptq==$autogptq_version -U && pip cache purge
 
-if [[ "$(printf '%s\n' "0.6.0" "$vllm_version" | sort -V | head -n1)" = "0.6.0" ]]; then
-    # vllm_version is >= 0.6.0
-    echo ">>Debug: Condition met: vllm_version >= 0.6.0, proceeding with installation..."
-    pip install --no-cache-dir vllm==$vllm_version && pip cache purge
-else
-    echo ">>Debug: Condition not met: vllm_version < 0.6.0, skipping installation. (vllm_version = $vllm_version)"
-fi
+pip install --no-cache-dir vllm==$vllm_version && pip cache purge
+
+#if [[ "$(printf '%s\n' "0.6.0" "$vllm_version" | sort -V | head -n1)" = "0.6.0" ]]; then
+#    # vllm_version is >= 0.6.0
+#    echo ">>Debug: Condition met: vllm_version >= 0.6.0, proceeding with installation..."
+#    pip install --no-cache-dir vllm==$vllm_version && pip cache purge
+#else
+#    echo ">>Debug: Condition not met: vllm_version < 0.6.0, skipping installation. (vllm_version = $vllm_version)"
+#fi
 
 # pip uninstall -y torch-scatter && TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5;8.0;8.6;8.9;9.0" pip install --no-cache-dir -U torch-scatter

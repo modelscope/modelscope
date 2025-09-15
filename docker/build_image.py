4ebfecb6cb65a04b5ae9b2da607ae16e802aa526
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-09-16 00:07:44 +0800

    update swift docker

--- docker/build_image.py
+++ docker/build_image.py
@@ -354,9 +354,9 @@ class SwiftImageBuilder(LLMImageBuilder):
             args.torchaudio_version = '2.7.1'
             args.torchvision_version = '0.22.1'
         if not args.vllm_version:
-            args.vllm_version = '0.10.0'
+            args.vllm_version = '0.10.1.1'
         if not args.lmdeploy_version:
-            args.lmdeploy_version = '0.9.2'
+            args.lmdeploy_version = '0.9.2.post1'
         if not args.flashattn_version:
             args.flashattn_version = '2.7.4.post1'
         return super().init_args(args)

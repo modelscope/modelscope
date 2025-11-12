78776141d7979263685e2712d847917cc083b5df
baymax591 <cbai@mail.nwpu.edu.cn>
2025-11-12 10:41:53 +0800

    add ascend dockerfile

--- docker/build_image.py
+++ docker/build_image.py
@@ -415,6 +415,75 @@ RUN pip install --no-cache-dir -U icecream soundfile pybind11 py-spy
         return os.system(f'docker push {image_tag2}')
 
 
+class AscendSwiftImageBuilder(SwiftImageBuilder):
+    def init_args(self, args) -> Any:
+        if not args.base_image:
+            # other vision search for: https://hub.docker.com/r/ascendai/cann/tags
+            args.base_image = "ascendai/cann:8.3.rc1-910-openeuler24.03-py3.11"
+        if not args.torch_version:
+            args.torch_version = "2.7.1"
+            args.torch_npu_version = "2.7.1"
+            args.torchvision_version = "0.22.1"
+        if not args.vllm_version:
+            args.vllm_version = "0.11.0"
+            args.vllm_ascend_version = "0.11.0-dev"
+        return super().init_args(args)
+
+    def generate_dockerfile(self) -> str:
+        meta_file = "./docker/install_ascend.sh"
+        with open("docker/Dockerfile.extra_install", "r") as f:
+            extra_content = f.read()
+            extra_content = extra_content.replace(
+                "{python_version}", self.args.python_version
+            )
+        extra_content += """
+RUN pip install --no-cache-dir -U icecream soundfile pybind11 py-spy
+"""
+        version_args = (
+            f"{self.args.torch_version} {self.args.torchvision_version} {self.args.torch_npu_version} "
+            f"{self.args.vllm_version} {self.args.vllm_ascend_version}"
+        )
+        with open('docker/Dockerfile.ubuntu', 'r') as f:
+            content = f.read()
+            content = content.replace('{base_image}', self.args.base_image)
+            content = content.replace('{extra_content}', extra_content)
+            content = content.replace('{meta_file}', meta_file)
+            content = content.replace('{version_args}', version_args)
+            content = content.replace('{cur_time}', formatted_time)
+            content = content.replace('{install_ms_deps}', 'False')
+            content = content.replace('{install_megatron_deps}', 'True')
+            content = content.replace('{torch_version}',
+                                      self.args.torch_version)
+            content = content.replace('{torchvision_version}',
+                                      self.args.torchvision_version)
+            content = content.replace('{torchaudio_version}',
+                                      self.args.torchaudio_version)
+            content = content.replace('{index_url}', '')
+            content = content.replace('{modelscope_branch}',
+                                      self.args.modelscope_branch)
+            content = content.replace('{swift_branch}', self.args.swift_branch)
+        return content
+
+    def image(self) -> str:
+        return (
+            f'{docker_registry}:ubuntu{self.args.ubuntu_version}-cuda{self.args.cuda_version}-'
+            f'{self.args.python_tag}-torch{self.args.torch_version}-{self.args.modelscope_version}-swift-test'
+        )
+
+    def push(self):
+        ret = os.system(f'docker push {self.image()}')
+        if ret != 0:
+            return ret
+        image_tag2 = (
+            f'{docker_registry}:ubuntu{self.args.ubuntu_version}-cuda{self.args.cuda_version}-'
+            f'{self.args.python_tag}-torch{self.args.torch_version}-'
+            f'{self.args.modelscope_version}-swift-{formatted_time}-test')
+        ret = os.system(f'docker tag {self.image()} {image_tag2}')
+        if ret != 0:
+            return ret
+        return os.system(f'docker push {image_tag2}')
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--base_image', type=str, default=None)
 parser.add_argument('--image_type', type=str)

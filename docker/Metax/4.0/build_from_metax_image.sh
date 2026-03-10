467a2206e30b017e40c99edc5098f84d7a8d1119
vx120 <57470515+vx120@users.noreply.github.com>
2026-03-10 16:11:49 +0800

    add metax dockerfile and its requirements for ms-swift (#1643)

---
+++ docker/Metax/4.0/build_from_metax_image.sh
@@ -0,0 +1,11 @@
+docker build \
+    --network host \
+    -f Dockerfile.with_metax_image \
+    -t swift:v4.0.0 \
+    --build-arg VLLM_VERSION=v0.11.2 \
+    --build-arg VLLM_METAX_VERSION=v0.11.2 \
+    --build-arg MEGATRON_VERSION=core_v0.15.0  \
+    --build-arg SWIFT_VERSION=v4.0.0 \
+    --progress=plain \
+    --no-cache \
+     .

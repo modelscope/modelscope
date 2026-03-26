56d201b9805ddd86a7e78a610dbe699c0f59445e
tastelikefeet <yuze.zyz@alibaba-inc.com>
2026-03-26 16:00:46 +0800

    bump version

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.35.1'
+__version__ = '1.35.2'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2026-03-15 00:00:00'
+__release_datetime__ = '2026-03-27 00:00:00'

af1cb9a1b0f1a0f99e94f0e016b76094a5a85cf7
班扬 <xingjun.wxj@alibaba-inc.com>
2026-04-29 01:54:42 +0800

    bump version

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.36.3'
+__version__ = '1.36.4'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2026-04-28 14:00:00'
+__release_datetime__ = '2026-04-29 02:00:00'

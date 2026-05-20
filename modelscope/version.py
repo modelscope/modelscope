7d89214eb12efc94f1b440e6199fa25cc9fc60b9
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-05-21 00:40:50 +0800

    bump version

--- modelscope/version.py
+++ modelscope/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '1.37.0'
+__version__ = '1.37.1'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2026-05-09 23:59:59'
+__release_datetime__ = '2026-05-20 23:59:59'
